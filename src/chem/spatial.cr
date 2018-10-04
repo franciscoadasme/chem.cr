f7fb860e130d5985a23710ea0ff90b5900e696b4
Francisco Adasme <francisco.adasme@gmail.com>
2018-10-04 14:46:07 -0500

    Added useful overloads

--- src/chem/spatial.cr
+++ src/chem/spatial.cr
@@ -7,19 +7,42 @@ module Chem::Spatial
     Math.atan2(v1.cross(v2).magnitude, v1.dot(v2)).degrees
   end
 
-  def dihedral(p1 : Vector, p2 : Vector, p3 : Vector, p4 : Vector) : Float64
-    v1, v2, v3 = p2 - p1, p3 - p2, p4 - p3
+  def angle(a1 : Atom, a2 : Atom, a3 : Atom) : Float64
+    angle a1.coords, a2.coords, a3.coords
+  end
+
+  def angle(p1 : Vector, p2 : Vector, p3 : Vector) : Float64
+    angle p2 - p1, p3 - p2
+  end
+
+  def dihedral(v1 : Vector, v2 : Vector, v3 : Vector) : Float64
     v12 = v1.cross v2
     angle = angle v12, v2.cross(v3)
     v12.dot(v3) < 0 ? -angle : angle
   end
 
-  def distance(v1 : Vector, v2 : Vector) : Float64
-    Math.sqrt squared_distance(v1, v2)
+  def dihedral(a1 : Atom, a2 : Atom, a3 : Atom, a4 : Atom) : Float64
+    dihedral a1.coords, a2.coords, a3.coords, a4.coords
+  end
+
+  def dihedral(p1 : Vector, p2 : Vector, p3 : Vector, p4 : Vector) : Float64
+    dihedral p2 - p1, p3 - p2, p4 - p3
+  end
+
+  def distance(a1 : Atom, a2 : Atom) : Float64
+    distance a1.coords, a2.coords
+  end
+
+  def distance(p1 : Vector, p2 : Vector) : Float64
+    Math.sqrt squared_distance(p1, p2)
+  end
+
+  def squared_distance(a1 : Atom, a2 : Atom) : Float64
+    squared_distance a1.coords, a2.coords
   end
 
   @[AlwaysInline]
-  def squared_distance(v1 : Vector, v2 : Vector) : Float64
-    (v1.x - v2.x)**2 + (v1.y - v2.y)**2 + (v1.z - v2.z)**2
+  def squared_distance(p1 : Vector, p2 : Vector) : Float64
+    (p1.x - p2.x)**2 + (p1.y - p2.y)**2 + (p1.z - p2.z)**2
   end
 end
