1f8ef0d4f7574b322cce937070d00112af38ca90
Francisco Adasme <francisco.adasme@gmail.com>
2018-11-08 20:39:53 -0600

    Added missing initializers

--- src/chem/protein/experiment.cr
+++ src/chem/protein/experiment.cr
@@ -17,5 +17,13 @@ module Chem::Protein
     getter pdb_accession : String
     getter resolution : Float64?
     getter title : String
+
+    def initialize(@title : String,
+                   @kind : Kind,
+                   @resolution : Float64?,
+                   @pdb_accession : String,
+                   @deposition_date : Time,
+                   @doi : String?)
+    end
   end
 end
