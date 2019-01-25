94e469418c53a3d82c0823e563f4f47317b04fb5
Francisco Adasme <francisco.adasme@gmail.com>
2019-01-25 15:08:14 -0600

    Updated `SecondaryStructure#dssp` method

--- src/chem/protein/secondary_structure.cr
+++ src/chem/protein/secondary_structure.cr
@@ -17,17 +17,44 @@ module Chem::Protein
       bend? || none? || turn?
     end
 
-    def dssp : Char
-      case self
-      when .bend?        then 'S'
-      when .beta_bridge? then 'B'
-      when .beta_strand? then 'E'
-      when .helix_3_10?  then 'G'
-      when .helix_alpha? then 'H'
-      when .helix_pi?    then 'I'
-      when .none?        then '0'
-      when .turn?        then 'T'
-      else                    '0'
+    # Returns the one-letter DSSP secondary structure code.
+    #
+    # If *simplified* is `true`, returns the simplified code (see below) instead.
+    #
+    # The DSSP assignment codes are:
+    #
+    # - `'H'` : α-helix (4-turn)
+    # - `'B'` : β-bridge
+    # - `'E'` : β-strand
+    # - `'G'` : 3₁₀-helix (3-turn)
+    # - `'I'` : π-helix (5-turn)
+    # - `'T'` : Turn
+    # - `'S'` : Bend
+    # - `'0'` : None (loops and irregular elements)
+    #
+    # The simplified DSSP codes are:
+    #
+    # - `'H'` : Helix. Either of the `'H'`, `'G'`, or `'I'` codes
+    # - `'E'` : Strand. Either of the `'E'`, or `'B'` codes
+    # - `'0'` : Coil. Either of the `'T'`, `'S'` or `'0'` codes
+    def dssp(*, simplified : Bool = false) : Char
+      if simplified
+        case self
+        when .helix_3_10?, .helix_alpha?, .helix_pi? then 'H'
+        when .beta_bridge?, .beta_strand?            then 'E'
+        else                                              '0'
+        end
+      else
+        case self
+        when .bend?        then 'S'
+        when .beta_bridge? then 'B'
+        when .beta_strand? then 'E'
+        when .helix_3_10?  then 'G'
+        when .helix_alpha? then 'H'
+        when .helix_pi?    then 'I'
+        when .turn?        then 'T'
+        else                    '0'
+        end
       end
     end
 
