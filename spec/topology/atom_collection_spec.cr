197a6172def81f46f3194f4beb98ef0579d9f701
Francisco Adasme <francisco.adasme@gmail.com>
2019-05-10 13:04:28 -0400

    Added fragment detection

---
+++ spec/topology/atom_collection_spec.cr
@@ -0,0 +1,26 @@
+require "../spec_helper"
+
+describe Chem::AtomCollection do
+  describe "#fragments" do
+    it "returns the fragments (1)" do
+      structure = Chem::Structure.read "spec/data/poscar/5e61--unwrapped.poscar"
+      builder = Chem::Topology::Builder.new structure
+      builder.guess_bonds_from_geometry
+      structure.fragments.map(&.size).should eq [100, 100]
+    end
+
+    it "returns the fragments (2)" do
+      structure = Chem::Structure.read "spec/data/poscar/5e61--unwrapped.poscar"
+      builder = Chem::Topology::Builder.new structure
+      builder.guess_bonds_from_geometry
+      structure.fragments.map(&.size).should eq [100, 100]
+    end
+
+    it "returns the fragments (3)" do
+      structure = Chem::Structure.read "spec/data/xyz/k2p_pore_b.xyz"
+      builder = Chem::Topology::Builder.new structure
+      builder.guess_bonds_from_geometry
+      structure.fragments.map(&.size).sort!.should eq [1, 1, 1, 1, 304, 334]
+    end
+  end
+end
--- src/chem/topology/atom_collection.cr
+++ src/chem/topology/atom_collection.cr
@@ -33,12 +33,29 @@ module Chem
       Spatial::CollectionProxy.new self
     end
 
+    def each_fragment(&block : AtomView ->) : Nil
+      fragment = Set(Atom).new
+      visited = Set(Atom).new
+      each_atom do |atom|
+        next if visited.includes? atom
+        collect_connected_atoms atom, fragment, visited
+        yield AtomView.new(fragment.to_a)
+        fragment.clear
+      end
+    end
+
     def formal_charge : Int32
       each_atom.sum &.formal_charge
     end
 
     def formal_charges : Array(Int32)
       each_atom.map(&.formal_charge).to_a
+    end
+
+    def fragments : Array(AtomView)
+      fragments = [] of AtomView
+      each_fragment { |fragment| fragments << fragment }
+      fragments
     end
 
     def transform(by transform : Spatial::AffineTransform) : self
@@ -51,6 +68,16 @@ module Chem
     def translate!(by vector : Spatial::Vector)
       each_atom do |atom|
         atom.coords += vector
+      end
+    end
+
+    private def collect_connected_atoms(atom : Atom,
+                                        fragment : Set(Atom),
+                                        visited : Set(Atom)) : Nil
+      fragment << atom
+      visited << atom
+      atom.bonded_atoms.each do |other|
+        collect_connected_atoms other, fragment, visited unless visited.includes? other
       end
     end
   end
