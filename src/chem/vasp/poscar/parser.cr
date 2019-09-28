3072053d169114bdc46a75b1f84c5ea723c5d6a8
Francisco Adasme <francisco.adasme@gmail.com>
2019-09-28 19:01:32 -0300

    Minor refactoring of poscar parser

--- src/chem/vasp/poscar/parser.cr
+++ src/chem/vasp/poscar/parser.cr
@@ -3,9 +3,6 @@ module Chem::VASP::Poscar
   class Parser < IO::Parser
     include IO::PullParser
 
-    @fractional = false
-    @lattice = Lattice[0, 0, 0]
-
     def initialize(@io : ::IO)
     end
 
@@ -21,13 +18,15 @@ module Chem::VASP::Poscar
       builder = Structure::Builder.new
       builder.title read_line
 
-      @lattice = builder.lattice self
+      lattice = builder.lattice self
       elements = parse_elements
       has_constraints = parse_selective_dynamics
-      parse_coordinate_system
+      fractional = parse_coordinate_system
 
       elements.each do |element|
-        atom = builder.atom of: element, at: read_coords
+        vec = read_vector
+        vec = fractional ? vec.to_cartesian(lattice) : vec * lattice.scale_factor
+        atom = builder.atom of: element, at: vec
         atom.constraint = Constraint.new self if has_constraints
       end
 
@@ -57,34 +56,21 @@ module Chem::VASP::Poscar
       Bool.new self
     end
 
-    # TODO check whether using a tuple and manual math would speed up the parsing
-    private def read_coords : Spatial::Vector
-      vec = read_vector
-      if @fractional
-        a = vec.x * @lattice.a
-        b = vec.y * @lattice.b
-        c = vec.z * @lattice.c
-        a + b + c
-      else
-        vec * @lattice.scale_factor
-      end
-    end
-
     def read_vector : Spatial::Vector
       Spatial::Vector.new self
     end
 
-    private def parse_coordinate_system : Nil
+    private def parse_coordinate_system : Bool
       skip_whitespace
       line = read_line
-      @fractional = case line[0].downcase
-                    when 'c', 'k' # cartesian
-                      false
-                    when 'd' # direct
-                      true
-                    else
-                      fail "Invalid coordinate type"
-                    end
+      case line[0].downcase
+      when 'c', 'k' # cartesian
+        false
+      when 'd' # direct
+        true
+      else
+        fail "Invalid coordinate type"
+      end
     end
   end
 end
