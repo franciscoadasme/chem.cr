97fdc848a6c02479663282fbf9c8deccbd86781e
Francisco Adasme <francisco.adasme@gmail.com>
2019-10-24 14:52:55 -0300

    Fixed typos

--- src/chem/topology/structure/builder.cr
+++ src/chem/topology/structure/builder.cr
@@ -77,12 +77,12 @@ module Chem
       Atom.new name, (@atom_serial += 1), coords, residue, **options
     end
 
-    def bond(name : String, other : String, order : Int = 1) : Bool
+    def bond(name : String, other : String, order : Int = 1) : Bond
       atom!(name).bonds.add atom!(other), order
     end
 
-    def bond(i : Int, j : Int, order : Int = 1) : Bool
       atoms[i].bonds.add atoms[j], order
+    def bond(i : Int, j : Int, order : Int = 1) : Bond
     end
 
     def build : Structure
