61271d8e431e758bea919b34335201395b915b0c
Francisco Adasme <francisco.adasme@gmail.com>
2019-12-20 14:28:02 -0300

    Refactored Spatial::Bounds to use Spatial::Basis
    This allows for non-orthogonal bounds, which is helpful to support non-orthogonal grids

--- spec/spatial/bounds_spec.cr
+++ spec/spatial/bounds_spec.cr
@@ -6,6 +6,7 @@ describe Chem::Spatial::Bounds do
       bounds = Bounds[1, 2, 3]
       bounds.origin.should eq V.origin
       bounds.size.should eq S[1, 2, 3]
+      bounds.angles.should eq({90, 90, 90})
     end
   end
 
@@ -14,6 +15,7 @@ describe Chem::Spatial::Bounds do
       bounds = Bounds.zero
       bounds.origin.should eq V.origin
       bounds.size.should eq S[0, 0, 0]
+      bounds.angles.should eq({0, 0, 0})
     end
   end
 
@@ -35,23 +37,23 @@ describe Chem::Spatial::Bounds do
     end
   end
 
-  describe "#*" do
-    it "multiplies a bounds's size by a number" do
-      (Bounds[1, 2, 3] * 3).should eq Bounds[3, 6, 9]
+  # describe "#*" do
+  #   it "multiplies a bounds's size by a number" do
+  #     (Bounds[1, 2, 3] * 3).should eq Bounds[3, 6, 9]
 
-      bounds = Bounds.new V[6, 1, 0], S[1.3, 6.5, 8.2]
-      (bounds * 2.5).should eq Bounds.new(V[6, 1, 0], S[3.25, 16.25, 20.5])
-    end
-  end
+  #     bounds = Bounds.new V[6, 1, 0], S[1.3, 6.5, 8.2]
+  #     (bounds * 2.5).should eq Bounds.new(V[6, 1, 0], S[3.25, 16.25, 20.5])
+  #   end
+  # end
 
-  describe "#/" do
-    it "divides a bounds's size by a number" do
-      (Bounds[3, 6, 9] / 3).should eq Bounds[1, 2, 3]
+  # describe "#/" do
+  #   it "divides a bounds's size by a number" do
+  #     (Bounds[3, 6, 9] / 3).should eq Bounds[1, 2, 3]
 
-      bounds = Bounds.new V[6, 1, 0], S[1.5, 5, 10]
-      (bounds / 0.25).should eq Bounds.new(V[6, 1, 0], S[6, 20, 40])
-    end
-  end
+  #     bounds = Bounds.new V[6, 1, 0], S[1.5, 5, 10]
+  #     (bounds / 0.25).should eq Bounds.new(V[6, 1, 0], S[6, 20, 40])
+  #   end
+  # end
 
   describe "#center" do
     it "returns the center of the bounds" do
@@ -61,14 +63,20 @@ describe Chem::Spatial::Bounds do
   end
 
   describe "#includes?" do
-    it "returns true when a vector is within bounds" do
+    it "tells if a vector is within the primary unit cell" do
       Bounds[10, 20, 30].includes?(V[1, 2, 3]).should be_true
       Bounds.new(V[1, 2, 3], S[6, 3, 23]).includes?(V[3, 2.1, 20]).should be_true
-    end
-
-    it "returns false when a vector is out of bounds" do
       Bounds[10, 20, 30].includes?(V[-1, 2, 3]).should be_false
       Bounds.new(V[1, 2, 3], S[6, 3, 23]).includes?(V[2.4, 1.8, 23.1]).should be_false
+    end
+
+    it "tells if a vector is within the primary unit cell (non-orthogonal)" do
+      lattice = Bounds.new S[23.803, 23.828, 5.387], 90, 90, 120
+      lattice.includes?(V[10, 20, 2]).should be_true
+      lattice.includes?(V[0, 0, 0]).should be_true
+      lattice.includes?(V[30, 30, 10]).should be_false
+      lattice.includes?(V[-3, 10, 2]).should be_true
+      lattice.includes?(V[-3, 2, 2]).should be_false
     end
   end
 
--- src/chem/spatial/bounds.cr
+++ src/chem/spatial/bounds.cr
@@ -1,45 +1,64 @@
 module Chem::Spatial
   struct Bounds
+    getter basis : Basis
     getter origin : Vector
-    getter size : Size
 
-    delegate :volume, to: @size
+    delegate a, alpha, angles, b, beta, c, gamma, i, j, k, size, to: @basis
 
-    def initialize(@origin : Vector, @size : Size)
+    def initialize(@origin : Vector, @basis : Basis)
     end
 
-    def self.[](x : Float64, y : Float64, z : Float64) : self
-      new Vector.origin, Size.new(x, y, z)
+    def self.new(origin : Vector, *args, **options) : self
+      new origin, Basis.new(*args, **options)
+    end
+
+    def self.new(*args, **options) : self
+      new Vector.origin, Basis.new(*args, **options)
     end
 
     def self.new(vmin : Vector, vmax : Vector) : self
-      new vmin, Size.new(vmax.x - vmin.x, vmax.y - vmin.y, vmax.z - vmin.z)
+      new vmin,
+        Vector[vmax.x - vmin.x, 0, 0],
+        Vector[0, vmax.y - vmin.y, 0],
+        Vector[0, 0, vmax.z - vmin.z]
+    end
+
+    def self.[](a : Float64, b : Float64, c : Float64) : self
+      new Vector.origin, Size[a, b, c]
     end
 
     def self.zero : self
-      new Vector.origin, Size.zero
+      new Vector.origin, Size[0, 0, 0]
     end
 
     {% for op in %w(+ -) %}
       def {{op.id}}(rhs : Vector) : self
-        Bounds.new @origin {{op.id}} rhs, @size
+        Bounds.new @origin {{op.id}} rhs, @basis
       end
     {% end %}
 
-    {% for op in %w(* /) %}
-      def {{op.id}}(rhs : Number) : self
-        Bounds.new @origin, @size {{op.id}} rhs
-      end
-    {% end %}
+    # {% for op in %w(* /) %}
+    #   def {{op.id}}(rhs : Number) : self
+    #     Bounds.new @origin, @a {{op.id}} rhs, @b {{op.id}} rhs, @c {{op.id}} rhs
+    #   end
+    # {% end %}
 
     def center : Vector
-      @origin + @size / 2
+      @origin + (@basis.i + @basis.j + @basis.k) * 0.5
     end
 
     def includes?(vec : Vector) : Bool
-      origin.x <= vec.x <= origin.x + size.x &&
-        origin.y <= vec.y <= origin.y + size.y &&
-        origin.z <= vec.z <= origin.z + size.z
+      vec -= @origin unless @origin.zero?
+      if alpha == 90 && beta == 90 && gamma == 90 && i.y == 0 && i.z == 0
+        0 <= vec.x <= a && 0 <= vec.y <= b && 0 <= vec.z <= c
+      else
+        vec = vec.to_fractional @basis
+        0 <= vec.x <= 1 && 0 <= vec.y <= 1 && 0 <= vec.z <= 1
+      end
+    end
+
+    def volume : Float64
+      @basis.i.dot @basis.j.cross(@basis.k)
     end
   end
 end
