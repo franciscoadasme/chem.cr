6993eb8c10ff364f103793b1ca4e16e3dc77e08d
Francisco Adasme <francisco.adasme@gmail.com>
2020-01-16 21:36:24 -0300

    Added support for Cube file format

---
+++ spec/io/cube_spec.cr
@@ -0,0 +1,65 @@
+require "../spec_helper"
+
+describe Chem::Cube::Parser do
+  it "parses a cube file" do
+    grid = Grid.from_cube "spec/data/cube/20.cube"
+    grid.dim.should eq({20, 20, 20})
+    grid.origin.should be_close V[-3.826155, -4.114553, -6.64407], 1e-6
+    grid.bounds.size.should be_close S[12.184834, 12.859271, 13.117308], 1e-6
+    grid[0, 0, 0].should eq 2.19227e-19
+    grid[-1, -1, -1].should eq 7.36329e-22
+    grid[1, 1, 5].should eq 1.61884e-14
+  end
+
+  it "parses a cube file header" do
+    info = Grid.info "spec/data/cube/20.cube"
+    info.dim.should eq({20, 20, 20})
+    info.bounds.origin.should be_close V[-3.826155, -4.114553, -6.64407], 1e-6
+    info.bounds.size.should be_close S[12.184834, 12.859271, 13.117308], 1e-6
+  end
+
+  it "fails when cube have multiple densities" do
+    io = IO::Memory.new <<-EOF
+      CPMD CUBE FILE.
+      OUTER LOOP: X, MIDDLE LOOP: Y, INNER LOOP: Z
+       -3    0.000000    0.000000    0.000000
+        1    0.283459    0.000000    0.000000
+        2    0.000000    0.283459    0.000000
+        2    0.000000    0.000000    0.283459
+        8    0.000000    5.570575    5.669178    5.593517
+        1    0.000000    5.562867    5.669178    7.428055
+        1    0.000000    7.340606    5.669178    5.111259
+       -0.25568E-04  0.59213E-05  0.81068E-05  0.10868E-04
+      EOF
+    expect_raises IO::Error, "not supported" do
+      Grid.from_cube io
+    end
+  end
+end
+
+describe Chem::Cube::Writer do
+  it "writes a grid" do
+    structure = Chem::Structure.build do
+      atom :Cu, V[2.317035, 3.50954, -0.79557], partial_charge: 29.0
+      atom :O, V[3.517299, 6.882575, -1.794666], partial_charge: 8.0
+      atom :H, V[3.658572, 8.310089, -0.667807], partial_charge: 1.0
+      atom :H, V[3.55781, 7.487509, -3.515277], partial_charge: 1.0
+      atom :O, V[0.522854, 0.396282, 0.154812], partial_charge: 8.0
+      atom :H, V[-0.123518, 0.047338, 1.824539], partial_charge: 1.0
+      atom :H, V[-0.230385, -0.775379, -1.022862], partial_charge: 1.0
+      atom :O, V[6.060706, 1.880214, -0.48988], partial_charge: 8.0
+      atom :H, V[6.473038, 0.17068, -0.010227], partial_charge: 1.0
+      atom :H, V[7.644308, 2.732568, -0.787247], partial_charge: 1.0
+      atom :O, V[2.14017, 4.657838, 2.815361], partial_charge: 8.0
+      atom :O, V[1.905012, 2.624361, -4.461808], partial_charge: 8.0
+      atom :H, V[0.470734, 2.905126, -5.555472], partial_charge: 1.0
+      atom :H, V[3.187141, 1.724891, -5.399287], partial_charge: 1.0
+      atom :H, V[3.511551, 4.405199, 3.993241], partial_charge: 1.0
+      atom :H, V[0.794769, 5.548665, 3.668909], partial_charge: 1.0
+    end
+
+    content = File.read "spec/data/cube/20.cube"
+    io = IO::Memory.new content
+    Grid.from_cube(io).to_cube(atoms: structure).should eq content
+  end
+end
---
+++ src/chem/io/formats/cube.cr
@@ -0,0 +1,84 @@
+module Chem::Cube
+  @[IO::FileType(format: Cube, ext: %w(cube))]
+  class Parser < Spatial::Grid::Parser
+    include IO::AsciiParser
+
+    BOHR_TO_ANGS = 0.529177210859
+
+    def info : Spatial::Grid::Info
+      skip_lines 2
+      n_atoms = read_int
+      raise ::IO::Error.new "Cube with multiple densities not supported" if n_atoms < 0
+
+      origin = read_vector * BOHR_TO_ANGS
+      nx, i = read_int, read_vector * BOHR_TO_ANGS
+      ny, j = read_int, read_vector * BOHR_TO_ANGS
+      nz, k = read_int, read_vector * BOHR_TO_ANGS
+      skip_lines n_atoms + 1
+      bounds = Spatial::Bounds.new origin, i * nx, j * ny, k * nz
+      Spatial::Grid::Info.new bounds, {nx, ny, nz}
+    end
+
+    def parse : Spatial::Grid
+      grid_info = info
+      Spatial::Grid.build(grid_info.dim, grid_info.bounds) do |buffer|
+        (grid_info.dim[0] * grid_info.dim[1] * grid_info.dim[2]).times do |i|
+          buffer[i] = read_float
+        end
+      end
+    end
+  end
+
+  @[IO::FileType(format: Cube, ext: %w(cube))]
+  class Writer < IO::Writer(Spatial::Grid)
+    ANGS_TO_BOHR = 1.88972612478289694072
+
+    def initialize(input : ::IO | Path | String,
+                   @atoms : AtomCollection,
+                   sync_close : Bool = false)
+      super input
+    end
+
+    def write(grid : Spatial::Grid) : Nil
+      check_open
+      write_header grid
+      write_atoms
+      write_array grid
+    end
+
+    private def write_array(grid : Spatial::Grid) : Nil
+      i = 0
+      grid.each do |ele|
+        i += 1
+        format "%13.5E", ele
+        @io << '\n' if i % 6 == 0
+      end
+      @io << '\n' unless i % 6 == 0
+    end
+
+    private def write_atoms : Nil
+      @atoms.each_atom do |atom|
+        formatl "%5d%12.6f%12.6f%12.6f%12.6f",
+          atom.atomic_number,
+          atom.partial_charge,
+          atom.x,
+          atom.y,
+          atom.z
+      end
+    end
+
+    private def write_header(grid : Spatial::Grid) : Nil
+      origin = grid.origin * ANGS_TO_BOHR
+      i = grid.bounds.i / grid.nx * ANGS_TO_BOHR
+      j = grid.bounds.j / grid.ny * ANGS_TO_BOHR
+      k = grid.bounds.k / grid.nz * ANGS_TO_BOHR
+
+      @io.puts "CUBE FILE GENERATED WITH CHEM.CR"
+      @io.puts "OUTER LOOP: X, MIDDLE LOOP: Y, INNER LOOP: Z"
+      formatl "%5d%12.6f%12.6f%12.6f", @atoms.n_atoms, origin.x, origin.y, origin.z
+      formatl "%5d%12.6f%12.6f%12.6f", grid.dim[0], i.x, i.y, i.z
+      formatl "%5d%12.6f%12.6f%12.6f", grid.dim[1], j.x, j.y, j.z
+      formatl "%5d%12.6f%12.6f%12.6f", grid.dim[2], k.x, k.y, k.z
+    end
+  end
+end
