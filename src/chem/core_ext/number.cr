f418a68ce812f42e4b030da89fe90d8d688d2856
Francisco Adasme <francisco.adasme@gmail.com>
2020-08-20 18:48:40 -0400

    Added number scaling methods

--- src/chem/core_ext/number.cr
+++ src/chem/core_ext/number.cr
@@ -21,4 +21,35 @@ abstract struct Number
   def radians
     self * DEGREE_TO_RADIAN
   end
+
+  # Scales the number into the given range. The result will be between
+  # zero and one.
+  #
+  # ```
+  # 10.scale(0, 100)   # => 0.1
+  # 2.5.scale(0, 5)    # 0.5
+  # 401.scale(50, 500) # => 0.78
+  # ```
+  def scale(min : Number, max : Number) : Float64
+    (self - min) / (max - min)
+  end
+
+  # :ditto:
+  def scale(range : Range(Number, Number)) : Float64
+    scale range.begin, range.end
+  end
+
+  # Reverts the scaling in the given range.
+  #
+  # 0.5.unscale(0, 100) # => 50.0
+  # 0.1.unscale(0, 5) # => 0.05
+  # 5.scale(-4, 20).unscale(-4, 20) # => 5.0
+  def unscale(min : Number, max : Number) : Float64
+    self * (max - min) + min
+  end
+
+  # :ditto:
+  def unscale(range : Range(Number, Number)) : Float64
+    unscale range.begin, range.end
+  end
 end
