5f8e214c076268a60ac712d49327c7f90ab7f683
Francisco Adasme <francisco.adasme@gmail.com>
2020-09-17 12:58:19 -0300

    Revert using min_by to return nil

--- src/chem/protein/quesso.cr
+++ src/chem/protein/quesso.cr
@@ -59,9 +59,9 @@ module Chem::Protein
         twist = h.theta.scale(0, 360)
         rise, twist = QUESSO.pes.walk rise, twist if rise > 0
         if !strict
-          QUESSO.pes.basin(rise, twist, check_proximity: false).sec || SecondaryStructure::None
+          QUESSO.pes.basin(rise, twist, check_proximity: false).try(&.sec) || SecondaryStructure::None
         elsif (curv = compute_curvature(residue)) && curv <= CURVATURE_CUTOFF
-          QUESSO.pes.basin(rise, twist).sec || SecondaryStructure::Uniform
+          QUESSO.pes.basin(rise, twist).try(&.sec) || SecondaryStructure::Uniform
         else
           SecondaryStructure::Bend
         end
@@ -182,13 +182,17 @@ module Chem::Protein
       end
 
       def basin(x : Float64, y : Float64, check_proximity : Bool = true) : Basin?
-        @basins.min_by do |basin|
-          if check_proximity && !basin.includes?(x, y)
-            Float64::MAX
-          else
-            (x - basin.x0)**2 + (y - basin.y0)**2
+        nearest_basin = nil
+        min_distance = Float64::MAX
+        @basins.each do |basin|
+          next if check_proximity && !basin.includes?(x, y)
+          d = (x - basin.x0)**2 + (y - basin.y0)**2
+          if d < min_distance
+            nearest_basin = basin
+            min_distance = d
           end
         end
+        nearest_basin
       end
 
       def diff(x : Float64, y : Float64) : Tuple(Float64, Float64)
