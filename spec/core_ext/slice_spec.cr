d0a4110f6526a338b23c11d67b4f906fe3ac0535
Francisco Adasme <francisco.adasme@gmail.com>
2020-10-20 12:38:26 -0300

    Added slice utility methods

---
+++ spec/core_ext/slice_spec.cr
@@ -0,0 +1,55 @@
+require "../spec_helper"
+
+describe Slice do
+  describe "#concat" do
+    it "concatenates two slices" do
+      slice = Slice[1, 2, 3, 4]
+      slice.concat(Slice[5, 6, 7, 8]).to_a.should eq (1..8).to_a
+    end
+
+    it "appends N elements from another slice" do
+      slice = Slice[1, 2, 3, 4]
+      slice.concat(Slice[5, 6, 7, 8], 2).to_a.should eq (1..6).to_a
+    end
+
+    it "appends N elements from a pointer" do
+      slice = Slice[1, 2, 3, 4]
+      ptr = Pointer.malloc(8) { |i| i + 10 }
+      slice.concat(ptr, 3).to_a.should eq [1, 2, 3, 4, 10, 11, 12]
+    end
+  end
+
+  describe "#take_while" do
+    it "returns a slice with elements" do
+      slice = "abc def".to_slice
+      slice.take_while(&.chr.letter?).should eq slice[0, 3]
+    end
+
+    it "returns itself" do
+      slice = "abcdef".to_slice
+      slice.take_while(&.chr.letter?).should eq slice
+    end
+
+    it "returns an empty slice" do
+      slice = "abcDEF".to_slice
+      slice.take_while(&.chr.uppercase?).should eq slice[0, 0]
+    end
+  end
+
+  describe "#unsafe_index" do
+    it "returns the index of the byte" do
+      slice = "abcdef".to_slice
+      slice.unsafe_index('d'.ord).should eq 3
+    end
+
+    it "returns the index of any of the bytes" do
+      slice = "abcdef".to_slice
+      slice.unsafe_index('x'.ord, 'y'.ord, 'b'.ord).should eq 1
+    end
+
+    it "returns nil byte is not found" do
+      slice = "abcdef".to_slice
+      slice.unsafe_index('x'.ord).should be_nil
+    end
+  end
+end
---
+++ src/chem/core_ext/slice.cr
@@ -0,0 +1,38 @@
+struct Slice(T)
+  def concat(other : self) : self
+    concat other.to_unsafe, other.size
+  end
+
+  def concat(other : self, count : Int) : self
+    concat other.to_unsafe, count
+  end
+
+  def concat(other : Pointer(T), count : Int) : self
+    return self unless count > 0
+    new_size = size + count
+    ptr = to_unsafe.realloc(new_size)
+    (ptr + size).copy_from other, count
+    Slice(T).new ptr, new_size
+  end
+
+  def take_while(& : T -> Bool) : self
+    each_with_index do |ele, i|
+      return Slice(T).new(@pointer, i) unless yield ele
+    end
+    self
+  end
+
+  def unsafe_index(byte : Int) : Int32?
+    if ptr = LibC.memchr(to_unsafe, byte, size)
+      (ptr - to_unsafe.as(Void*)).to_i
+    end
+  end
+
+  def unsafe_index(*bytes : Int) : Int32?
+    bytes.each do |byte|
+      if i = unsafe_index(byte)
+        return i
+      end
+    end
+  end
+end
