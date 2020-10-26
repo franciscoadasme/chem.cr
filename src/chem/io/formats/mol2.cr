848e663bbe1780e6c210466ffc2a8992a01112f2
Francisco Adasme <francisco.adasme@gmail.com>
2020-10-26 12:05:48 -0300

    Use new text parser in Mol2

--- src/chem/io/formats/mol2.cr
+++ src/chem/io/formats/mol2.cr
@@ -1,10 +1,4 @@
 module Chem::Mol2
-  enum RecordType
-    Molecule
-    Atom
-    Bond
-  end
-
   @[IO::FileType(format: Mol2, ext: %w(mol2))]
   class Writer < IO::Writer(AtomCollection)
     @atom_table = {} of Atom => Int32
@@ -99,116 +93,101 @@ module Chem::Mol2
 
   @[IO::FileType(format: Mol2, ext: %w(mol2))]
   class Reader < Structure::Reader
-    include IO::PullParser
+    TAG          = "@<TRIPOS>"
+    TAG_ATOMS    = "@<TRIPOS>ATOM"
+    TAG_BONDS    = "@<TRIPOS>BOND"
+    TAG_MOLECULE = "@<TRIPOS>MOLECULE"
+
+    @builder = uninitialized Structure::Builder
+    @include_charges = true
+    @n_atoms = 0
+    @n_bonds = 0
+    @title = ""
 
     def next : Structure | Iterator::Stop
-      skip_to_record :molecule
-      eof? ? stop : parse_next
+      skip_to_tag
+      !@parser.eof? ? read_next : stop
     end
 
     def skip_structure : Nil
-      skip_line if record? :molecule
-      skip_to_record :molecule
+      @parser.skip_line if @parser.skip_whitespace.check(TAG_MOLECULE)
+      skip_to_tag TAG_MOLECULE
     end
 
-    private def guess_bond_order(bond_type : String) : Int32
-      case bond_type
-      when "am", "ar", "du"
-        1
-      when "un", "nc"
-        0
-      else
-        bond_type.to_i
+    private def read_atom : Atom
+      serial = @parser.read_int
+      name = @parser.skip_spaces.scan 'A'..'z', '0'..'9'
+      coords = @parser.read_vector
+      element = PeriodicTable[@parser.read_word]
+      @parser.skip('.').skip_word # skip atom type
+      unless @parser.eol?
+        resid = @parser.read_int
+        resname = @parser.skip_spaces.read_word
+        @builder.residue resname[..2], resid
+        charge = @parser.read_float if @include_charges
       end
+      @parser.skip_line
+      @builder.atom name, coords, element: element, partial_charge: (charge || 0.0)
     end
 
-    private def next_record : RecordType?
-      until eof?
-        if record_type = read_record
-          return record_type
+    private def read_bond : Nil
+      serial = @parser.read_int
+      i = @parser.read_int - 1
+      j = @parser.read_int - 1
+      bond_type = @parser.skip_spaces.scan('a'..'z', '0'..'9')
+      bond_order = case bond_type
+                   when "1", "2", "3"    then bond_type.to_i
+                   when "am", "ar", "du" then 1
+                   else                       0
+                   end
+      @parser.skip_line
+      @builder.bond i, j, bond_order, aromatic: bond_type == "ar" if bond_order > 0
+    end
+
+    private def read_header : Nil
+      if @parser.check(TAG_MOLECULE)
+        @parser.skip_line
+      else
+        parse_exception "Invalid tag for structure"
+      end
+
+      @title = @parser.read_line.strip
+      @n_atoms = @parser.read_int
+      @n_bonds = @parser.read_int
+      @parser.skip_line
+      @parser.skip_line
+      @include_charges = @parser.read_line.strip != "NO_CHARGES"
+    end
+
+    private def read_next : Structure
+      read_header
+      @builder = Structure::Builder.new guess_topology: false
+      @builder.title @title
+      until @parser.eof?
+        case @parser.skip_whitespace
+        when .check(TAG_ATOMS)
+          @parser.skip_line
+          @n_atoms.times { read_atom }
+        when .check(TAG_BONDS)
+          @parser.skip_line
+          @n_bonds.times { read_bond }
+        when .check(TAG_MOLECULE)
+          break
         else
-          skip_line
+          @parser.skip_line
         end
       end
+      @builder.build
     end
 
-    private def parse_atom(builder : Structure::Builder) : Nil
-      skip_whitespace
-      skip_index
-      name = scan_in_set "a-zA-Z0-9"
-      coords = read_vector
-      element = read_element
-      skip('.').skip_in_set("A-z0-9").skip_spaces # skip atom type
-      unless check(&.whitespace?)
-        resid = read_int
-        resname = skip_spaces.scan_in_set "A-z0-9"
-        builder.residue resname[..2], resid
-      end
-      charge = read_float unless skip_spaces.check(&.whitespace?)
-      skip_line
-      builder.atom name, coords, element: element, partial_charge: (charge || 0.0)
+    private def skip_to_tag : Nil
+      skip_to_tag TAG
     end
 
-    private def parse_bond(builder : Structure::Builder) : Nil
-      skip_index
-      i = read_int - 1
-      j = read_int - 1
-      bond_type = skip_spaces.scan(/[a-z0-9]+/).to_s
-      bond_order = guess_bond_order bond_type
-      builder.bond i, j, bond_order, aromatic: bond_type == "ar" if bond_order > 0
-      skip_line
-    end
-
-    private def parse_next : Structure
-      Structure.build(guess_topology: false) do |builder|
-        skip_line
-        builder.title read_line.strip
-        n_atoms = read_int
-        n_bonds = read_int
-        skip_line
-
-        while name = next_record
-          case name
-          when .atom?
-            n_atoms.times { parse_atom builder }
-          when .bond?
-            n_bonds.times { parse_bond builder }
-          when .molecule?
-            @io.pos = @prev_pos
-            break
-          end
-        end
-      end
-    end
-
-    private def read_element : Element
-      PeriodicTable[skip_spaces.scan_in_set("A-z")]
-    end
-
-    private def read_record : RecordType?
-      skip_whitespace
-      return unless check "@<TRIPOS>"
-      name = read { skip(9).read_line.rstrip.downcase }
-      RecordType.parse? name
-    end
-
-    private def record?(type : RecordType) : Bool
-      if record_type = read_record
-        @io.pos = @prev_pos
-        record_type == type
-      else
-        false
-      end
-    end
-
-    private def skip_index : self
-      skip_spaces.skip_in_set("0-9").skip_spaces
-    end
-
-    private def skip_to_record(type : RecordType) : Nil
-      until eof?
-        break if record? type
-        skip_line
+    private def skip_to_tag(tag : String) : Nil
+      until @parser.eof?
+        break if @parser.skip_whitespace.check(tag)
+        @parser.skip_line
       end
     end
   end
