4f13d5a10c14486528d8ca7683b5ac9bf506d702
Francisco Adasme <francisco.adasme@gmail.com>
2021-04-25 13:22:58 -0400

    XYZ docs

--- src/chem/formats/xyz.cr
+++ src/chem/formats/xyz.cr
@@ -1,5 +1,99 @@
+# The XYZ module provides capabilities for reading and writing in the
+# XYZ file format.
+#
+# The XYZ file format is a text-based free format that encodes molecular
+# structure. There is no formal standard specification, but the most
+# common format is as follows:
+#
+# ```text
+# <number of atoms>
+# comment line
+# <element> <X> <Y> <Z>
+# ...
+# ```
+#
+# Elements are specified by their symbols, and atom coordinates are
+# free-form. The units are expected in ångströms. Additional columns may
+# exist, but these are ignored. A XYZ file can hold multiple blocks,
+# where each one of them is decoded into a `Structure` instance. Further
+# details can be found at the [XYZ file format
+# specification](http://openbabel.org/wiki/XYZ_%28format%29) of
+# OpenBabel.
+#
+# Registered file extensions for XYZ are: `.xyz`.
+#
+# ### Reading XYZ files
+#
+# The `XYZ::Reader` class reads XYZ entries sequentially from an `IO` or
+# file. Use either the `#each` method to yield every entry or
+# `#read_next` method to read the next entry only.
+#
+# ```
+# XYZ::Reader.open("/path/to/xyz") do |reader|
+#   reader.each do |structure|
+#     ...
+#   end
+#
+#   # or
+#
+#   while structure = reader.read_next
+#     ...
+#   end
+# end
+# ```
+#
+# Alternatively, use the convenience `Structure.from_xyz` and
+# `Array.from_xyz` methods to read the first or all entries in a XYZ
+# file, respectively.
+#
+# ```
+# Structure.from_xyz "/path/to/xyz" # => <Structure ...>
+# # or
+# Array(Structure).from_xyz "/path/to/xyz" # => [<Structure ...>, ...]
+# ```
+#
+# Similarly, the general `Structure#read` method can be used to read a
+# XYZ file, but the file format is determined on runtime.
+#
+# ### Writing XYZ files
+#
+# The `XYZ::Writer` class writes XYZ entries sequentially to an `IO` or
+# file. Use the `#<<` method to write an instance of a compatible type.
+# It can be called multiple times.
+#
+# ```
+# XYZ::Writer.open("/path/to/xyz") do |writer|
+#   writer.write structure1
+#   writer.write structure2
+#   ...
+# end
+# ```
+#
+# Alternatively, use the convenience `Structure#to_xyz` or
+# `Array#to_xyz` methods to write a single or multiple entries to a XYZ
+# file.
+#
+# ```
+# structure = Structure.build do |builder|
+#   ...
+# end
+# structure.to_xyz "/path/to/xyz"
+#
+# # or
+#
+# [structure1, structure2, ...].to_xyz "/path/to/xyz"
+# ```
 @[Chem::FileType(ext: %w(xyz))]
 module Chem::XYZ
+  # Writes entries sequentially to a XYZ file.
+  #
+  # ```
+  # XYZ::Writer.open("/path/to/xyz") do |writer|
+  #   writer << structure1
+  #   writer << structure2
+  #   ...
+  # end
+  # ```
   class Writer
     include FormatWriter(AtomCollection)
     include MultiFormatWriter(AtomCollection)
@@ -21,11 +115,30 @@ module Chem::XYZ
     end
   end
 
+  # Reads the entries sequentially in a XYZ file. Use either the `#each`
+  # method to yield every entry or `#read_next` method to read the next
+  # entry only.
+  #
+  # ```
+  # XYZ::Reader.open("/path/to/xyz") do |reader|
+  #   reader.each do |structure|
+  #     ...
+  #   end
+  #
+  #   # or
+  #
+  #   while structure = reader.read_next
+  #     ...
+  #   end
+  # end
+  # ```
   class Reader
     include FormatReader(Structure)
     include MultiFormatReader(Structure)
     include TextFormatReader
 
+    # Triggers bond and topology perception after reading. See
+    # `Structure::Builder#build` for more information.
     needs guess_topology : Bool = true
 
     def read_next : Structure?
