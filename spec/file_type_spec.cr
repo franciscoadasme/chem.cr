39ee820bb062c315a6147f8db75d14db14367afe
Francisco Adasme <francisco.adasme@gmail.com>
2021-04-26 17:34:50 -0400

    File type specs

---
+++ spec/file_type_spec.cr
@@ -0,0 +1,79 @@
+require "./spec_helper"
+
+describe Chem::FileType do
+  it "checks for reader/writer" do
+    assert_error <<-EOS, "Chem::Foo does not declare readers or writers"
+      @[Chem::FileType]
+      module Chem::Foo; end
+      EOS
+  end
+
+  it "checks for missing encoded types" do
+    assert_error <<-EOS, "Chem::Foo does not declare readers or writers"
+      @[Chem::FileType]
+      module Chem::Foo
+        class Reader; end
+      end
+      EOS
+  end
+
+  it "checks for duplicate file formats" do
+    message = "Chem::Bar::Foo declares file format Foo, but it is already \
+               declared by Chem::Foo"
+    assert_error <<-EOS, message
+      @[Chem::FileType]
+      module Chem::Foo
+        class Reader
+          include Chem::FormatReader(Int32)
+        end
+      end
+
+      @[Chem::FileType]
+      module Chem::Bar::Foo
+        class Reader
+          include Chem::FormatReader(Int32)
+        end
+      end
+      EOS
+  end
+
+  it "checks for duplicate file extensions" do
+    message = ".log extension declared in FileType annotation for Chem::Bar is \
+               already associated with file format Foo via Chem::Foo"
+    assert_error <<-EOS, message
+      @[Chem::FileType(ext: %w(log))]
+      module Chem::Foo
+        class Reader
+          include Chem::FormatReader(String)
+        end
+      end
+
+      @[Chem::FileType(ext: %w(log))]
+      module Chem::Bar
+        class Reader
+          include Chem::FormatReader(String)
+        end
+      end
+      EOS
+  end
+
+  it "checks for duplicate file name" do
+    message = "Filename FOO* declared in FileType annotation for Chem::Bar is \
+               already associated with file format Foo via Chem::Foo"
+    assert_error <<-EOS, message
+      @[Chem::FileType(names: %w(FOO))]
+      module Chem::Foo
+        class Reader
+          include Chem::FormatReader(String)
+        end
+      end
+
+      @[Chem::FileType(names: %w(FOO*))]
+      module Chem::Bar
+        class Reader
+          include Chem::FormatReader(String)
+        end
+      end
+      EOS
+  end
+end
