262c5ae9aba2ca825909c547e39be1aed120dc3f
Francisco Adasme <francisco.adasme@gmail.com>
2021-07-09 23:09:51 -0400

    Add IO::Wrapper

---
+++ spec/core_ext/io/wrapper_spec.cr
@@ -0,0 +1,178 @@
+require "../../spec_helper"
+
+describe IO::Wrapper do
+  it "fails on invalid first argument" do
+    message = "First argument of `BadArgumentIO#initialize` must be \
+               `io : IO`, not `foo : String`"
+    assert_error <<-EOS, message
+      class BadArgumentIO
+        include IO::Wrapper
+
+        def initialize(foo : String, bar : Int32); end
+      end
+      EOS
+  end
+
+  it "fails on missing argument sync_close" do
+    message = "Missing argument `sync_close : Bool = false` in \
+               `MissingArgumentIO#initialize`"
+    assert_error <<-EOS, message
+      class MissingArgumentIO
+        include IO::Wrapper
+
+        def initialize(io : IO); end
+      end
+      EOS
+  end
+
+  it "fails on invalid type of argument sync_close" do
+    message = "Argument `sync_close` of `BadArgumentTypeIO#initialize` \
+               must be `sync_close : Bool = false`, not `sync_close = 1`"
+    assert_error <<-EOS, message
+      class BadArgumentTypeIO
+        include IO::Wrapper
+
+        def initialize(io : IO, sync_close = 1); end
+      end
+      EOS
+  end
+
+  describe "#initialize" do
+    it "initializes from an IO" do
+      io = LineIO.new IO::Memory.new, chomp: true, sync_close: true
+      io.chomp?.should be_true
+      io.sync_close.should be_true
+    end
+
+    it "initializes from an IO (no options)" do
+      io = SimpleIO.new IO::Memory.new
+      io.closed?.should be_false
+      io.close
+    end
+
+    it "initializes from a path" do
+      tempfile = File.tempfile(".txt")
+      io = LineIO.new tempfile.path, chomp: true
+      io.chomp?.should be_true
+      io.sync_close.should be_true
+      io.close
+      tempfile.delete
+    end
+
+    it "initializes from a path (no options)" do
+      tempfile = File.tempfile(".txt")
+      io = SimpleIO.new tempfile.path
+      io.closed?.should be_false
+      io.close
+      tempfile.delete
+    end
+
+    it "initializes from a path in write mode" do
+      tempfile = File.tempfile(".txt")
+      io = WriteIO.new tempfile.path
+      io << "abc"
+      io.close
+      tempfile.delete
+    end
+  end
+
+  describe ".open" do
+    it "opens an IO, yield it, closes it, and returns the last value" do
+      io = IO::Memory.new "abc\ndef\n"
+      line = LineIO.open(io, chomp: true, sync_close: true) do |line_io|
+        line_io.next_line
+        line_io.next_line
+      end
+      io.closed?.should be_true
+      line.should eq "def"
+    end
+
+    it "opens an IO (no options)" do
+      io = IO::Memory.new "abc\ndef\n"
+      line = SimpleIO.open(io, sync_close: true) do |simple_io|
+        simple_io.gets
+      end
+      io.closed?.should be_true
+      line.should eq "abc"
+    end
+
+    it "opens a file, yield it, closes it, and returns the last value" do
+      tempfile = File.tempfile(".txt") do |io|
+        io.puts "abc"
+        io.puts "def"
+      end
+      line = LineIO.open(tempfile.path, chomp: true) do |line_io|
+        line_io.next_line
+        line_io.next_line
+      end
+      line.should eq "def"
+      tempfile.delete
+    end
+
+    it "opens an IO (no options)" do
+      tempfile = File.tempfile(".txt") do |io|
+        io.puts "abc"
+        io.puts "def"
+      end
+      line = SimpleIO.open(tempfile.path) do |simple_io|
+        simple_io.gets
+      end
+      line.should eq "abc"
+      tempfile.delete
+    end
+  end
+
+  describe "#closed?" do
+    it "tells if the IO is closed" do
+      io = LineIO.new IO::Memory.new
+      io.closed?.should be_false
+      io.close
+      io.closed?.should be_true
+    end
+  end
+
+  describe "#close" do
+    it "closes the IO" do
+      io = LineIO.new IO::Memory.new
+      io.close
+      expect_raises IO::Error, "Closed IO" do
+        io.next_line
+      end
+    end
+
+    it "closes the underlying IO" do
+      io = IO::Memory.new
+      fake_io = LineIO.new io, sync_close: true
+      fake_io.close
+      io.closed?.should be_true
+    end
+  end
+end
+
+private class LineIO
+  include IO::Wrapper
+
+  getter? chomp : Bool
+
+  def initialize(@io : IO, @chomp = false, @sync_close : Bool = false)
+  end
+
+  def next_line : String?
+    check_open
+    @io.gets chomp: chomp?
+  end
+end
+
+private class SimpleIO
+  include IO::Wrapper
+
+  delegate gets, to: @io
+end
+
+private class WriteIO
+  include IO::Wrapper
+
+  FILE_MODE = "w"
+
+  delegate :<<, to: @io
+end
---
+++ src/chem/core_ext/io/wrapper.cr
@@ -0,0 +1,186 @@
+# Defines a common interface for types enclosing an `IO`.
+#
+# A default constructor is defined by this module, which may be
+# overriden by the including type. The latter must initialize the `@io`
+# (stores the enclosed IO) and `@sync_close` instance variables.
+#
+# Additionally, the following convenience methods are the generated on
+# compile time:
+#
+# * `self.new(path : String | Path, ...)`
+# * `self.open(io : IO, ..., sync_close : Bool = false, & : self ->)`
+# * `self.open(path : String | Path, ..., & : self ->)`
+#
+# Ellipsis stand for the positional and named arguments declared in the
+# custom constructor, if any.
+#
+# ```
+# class LineIO
+#   include IO::Wrapper
+#
+#   getter? chomp : Bool
+#
+#   def initialize(@io : IO, @chomp = false, @sync_close : Bool = false)
+#   end
+#
+#   def next_line : String?
+#     check_open
+#     @io.gets chomp: chomp?
+#   end
+# end
+#
+# io = IO::Memory.new "abc\ndef\n"
+# line_io = LineIO.new io, chomp: true, sync_close: false
+# line_io.closed?    # => false
+# line_io.sync_close # => false
+#
+# line_io.next_line # => "abc"
+# line_io.next_line # => "def"
+# line_io.next_line # => nil
+#
+# line_io.sync_close = true
+# line_io.close
+# line_io.closed? # => true
+# io.closed?      # => true
+# ```
+#
+# Alternatively, the above could be written as:
+#
+# ```
+# line = LineIO.open(io, chomp: true) do |line_io|
+#   line_io.next_line
+#   line_io.next_line
+# end
+# line # => "def"
+# ```
+module ::IO::Wrapper
+  # File open mode. May be overriden by including types.
+  FILE_MODE = "r"
+
+  # Returns `true` if this object is closed.
+  getter? closed : Bool = false
+
+  # Whether to close the enclosed `IO` when closing this object.
+  property sync_close : Bool = false
+
+  # Creates a new object from the given *io*.
+  def initialize(@io : IO, @sync_close : Bool = false); end
+
+  # Closes this object. If *sync_close* is true, it will also close the
+  # enclosed `IO`.
+  def close
+    return if @closed
+    @closed = true
+    @io.close if @sync_close
+  end
+
+  # Raises an `IO::Error` if the enclosed `IO` is closed.
+  protected def check_open : Nil
+    raise IO::Error.new "Closed IO" if closed?
+  end
+
+  macro included
+    {% if @type.class? || @type.struct? %}
+      macro finished
+        generate_convenience_constructors
+      end
+    {% elsif @type.module? %}
+      macro included
+        macro finished
+          generate_convenience_constructors
+        end
+      end
+    {% end %}
+  end
+
+  # Defines convenience `initialize` and `open` methods mirroring the
+  # designated constructor. Positional and named arguments are copied
+  # from it except for `io` and `sync_close`.
+  #
+  # NOTE: Do not call this method directly.
+  private macro generate_convenience_constructors
+    {% args = [] of Nil %}
+    {% if method = @type.methods.find(&.name.==("initialize")) %}
+      {% if method.args[0].id != "io : IO" %}
+        {% method.raise "First argument of `#{@type}#initialize` must be \
+                         `io : IO`, not `#{method.args[0]}`" %}
+      {% end %}
+
+      {% if arg = method.args.find(&.name.==("sync_close")) %}
+        {% if arg.id != "sync_close : Bool = false" %}
+          {% method.raise "Argument `sync_close` of `#{@type}#initialize` \
+                           must be `sync_close : Bool = false`, not `#{arg}`" %}
+        {% end %}
+      {% else %}
+        {% method.raise "Missing argument `sync_close : Bool = false` in \
+                         `#{@type}#initialize`" %}
+      {% end %}
+
+      {% args = method.args.select do |arg|
+           !%w(io sync_close).includes? arg.name.stringify
+         end %}
+    {% end %}
+
+    # Creates a new object from the given *path*. Positional and named
+    # arguments are forwarded to the designated constructor.
+    #
+    # An `IO` object will be created and open from the given *path*,
+    # which will be closed when closing this object (`sync_close =
+    # true`). The file open mode is specified by the `FILE_MODE`
+    # constant.
+    def self.new(
+      path : Path | String,
+      {% for arg in args %}
+        {{arg}},
+      {% end %}
+    ) : self
+      new File.new(path, FILE_MODE),
+        {% for arg in args %}
+          {{arg.is_a?(TypeDeclaration) ? arg.var : arg.name}},
+        {% end %}
+        sync_close: true
+    end
+
+    # Creates a new object from the given *io*, yields it to the given
+    # block, and closes it at the end. Positional and named arguments
+    # are forwarded to the constructor.
+    def self.open(
+      io : IO,
+      {% for arg in args %}
+        {{arg}},
+      {% end %}
+      sync_close : Bool = false,
+      & : self ->
+    )
+      io = new io,
+        {% for arg in args %}
+          {{arg.is_a?(TypeDeclaration) ? arg.var : arg.name}},
+        {% end %}
+        sync_close: sync_close
+      yield io ensure io.close
+    end
+
+    # Creates a new object from the given *path* yields it to the
+    # given block, and closes it at the end. Positional and named
+    # arguments are forwarded to the constructor.
+    #
+    # An `IO` object will be created and open from the given *path*,
+    # which will be closed when closing this object (`sync_close =
+    # true`). The file open mode is specified by the `FILE_MODE`
+    # constant.
+    def self.open(
+      path : Path | String,
+      {% for arg in args %}
+        {{arg}},
+      {% end %}
+      & : self ->
+    )
+      io = new path{{",".id unless args.empty?}}
+        {% for arg, i in args %}
+          {{arg.is_a?(TypeDeclaration) ? arg.var : arg.name}}
+          {{",".id if i < args.size - 1}}
+        {% end %}
+      yield io ensure io.close
+    end
+  end
+end
