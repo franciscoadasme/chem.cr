566fb8fd7218be661527d4065c13fd73e7716e33
Francisco Adasme <francisco.adasme@gmail.com>
2021-09-23 13:49:35 -0300

    Add location to ParseException

--- src/chem.cr
+++ src/chem.cr
@@ -34,5 +34,37 @@ require "./chem/formats/**"
 module Chem
   class Error < Exception; end
 
-  class ParseException < Exception; end
+  class ParseException < Exception
+    @line : String?
+    @location : Tuple(Int32, Int32, Int32)?
+
+    def initialize(@message : String); end
+
+    def initialize(@message : String,
+                   @path : String?,
+                   @line : String,
+                   @location : Tuple(Int32, Int32, Int32))
+    end
+
+    def inspect_with_location : String
+      String.build do |io|
+        inspect_with_location io
+      end
+    end
+
+    def inspect_with_location(io : IO) : Nil
+      io << "Found a parsing issue"
+      io << " in " << @path if @path
+      io << ':' << '\n' << '\n'
+      if location = @location
+        line_number, column_number, cursor_size = location
+        io << ' ' << line_number << " | " << @line << '\n'
+        (column_number + line_number.to_s.bytesize + 4).times { io << ' ' }
+        cursor_size.times { io << '^' }
+        io << '\n'
+      end
+      io << "Error: " << @message
+      io.flush
+    end
+  end
 end
