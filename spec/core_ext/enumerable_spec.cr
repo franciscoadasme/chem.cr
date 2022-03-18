eec3475f6fc90eef25e5a976dfc8a5f280a01ce7
Francisco Adasme <francisco.adasme@gmail.com>
2022-03-18 14:21:33 -0300

    Add average methods to Enumerable

--- spec/core_ext/enumerable_spec.cr
+++ spec/core_ext/enumerable_spec.cr
@@ -1,6 +1,29 @@
 require "../spec_helper"
 
 describe Enumerable do
+  describe "#average" do
+    it "returns the weighted average" do
+      (1..9).average((0..8).to_a).should eq 20 / 3
+      (1..9).to_a.average((0..8).to_a).should eq 20 / 3
+    end
+
+    it "returns the weighted average with block" do
+      (1..9).average((0..8).to_a, &.**(2)).should eq 145 / 3
+      (1..9).to_a.average((0..8).to_a, &.**(2)).should eq 145 / 3
+      ["Alice", "Bob"].average([7, 2], &.size).should eq 4.555555555555555
+      ('a'..'z').average((0..25).to_a, &.ord).should eq 114
+    end
+
+    it "raises if incompatible sizes" do
+      expect_raises(ArgumentError) { (1..5).average([0, 1]) }
+      expect_raises(ArgumentError) { [1, 2, 3, 4].average([0, 1, 2]) }
+    end
+
+    it "raises if empty" do
+      expect_raises(Enumerable::EmptyError) { ([] of Int32).average([1, 1, 1]) }
+    end
+  end
+
   describe "#mean" do
     it "returns the mean" do
       (1..40).mean.should eq 20.5
--- src/chem/core_ext/enumerable.cr
+++ src/chem/core_ext/enumerable.cr
@@ -1,4 +1,67 @@
 module Enumerable(T)
+  # Returns the weighted average of the elements in the collection.
+  # Raises `EmptyError` if the collection is empty or `ArgumentError` if
+  # *weights* has a different number of elements.
+  #
+  # Expects all element types to respond to `#+` and `#/` methods.
+  #
+  # ```
+  # [1, 2, 3, 4, 5, 6].average((1..6).to_a) # => 4.333333333333333
+  # (1..6).average((1..6).to_a)             # => 4.333333333333333
+  # ([] of Int32).average([1, 1, 1])        # raises EmptyError
+  # (1..6).average([1, 1])                  # raises ArgumentError
+  # ```
+  #
+  # NOTE: This method calls `.additive_identity` on the element type and
+  # *weights*' element type to determine the type of the intermediate
+  # sum values.
+  def average(weights : Indexable(Number))
+    average weights, &.itself
+  end
+
+  # Returns the weighted average of the results of the passed block for
+  # each element in the collection. Raises `EmptyError` if the
+  # collection is empty or `ArgumentError` if *weights* has a different
+  # number of elements.
+  #
+  # Expects all element types to respond to `#+` and `#/` methods.
+  #
+  # ```
+  # ["Alice", "Bob"].average([7, 2], &.size)    # => 4.555555555555555
+  # ('a'..'z').average((0..25).to_a, &.ord)     # => 4.333333333333333
+  # ([] of String).average([1, 1], &.size)      # raises EmptyError
+  # ["Alice", "Bob"].average([1, 1, 1], &.size) # raises ArgumentError
+  # ```
+  #
+  # NOTE: This method calls `.additive_identity` on the yielded type and
+  # *weights*' element type to determine the type of the intermediate
+  # sum values.
+  def average(weights : Indexable(Number), & : T -> _)
+    memo = typeof(yield Enumerable.element_type(self)).additive_identity
+    total = typeof(weights.unsafe_fetch(0)).additive_identity
+    if self.responds_to?(:size)
+      raise EmptyError.new unless size > 0
+      raise ArgumentError.new("Incompatible size") unless weights.size == size
+      each_with_index do |ele, i|
+        weight = weights.unsafe_fetch(i) # safe since they've the same size
+        value = yield ele
+        memo += value * weight
+        total += weight
+      end
+    else
+      count = 0
+      each_with_index do |ele, i|
+        weight = weights[i] # cannot ensure that exists
+        value = yield ele
+        memo += value * weight
+        total += weight
+        count += 1
+      end
+      raise ArgumentError.new("Incompatible size") unless count == weights.size
+    end
+    memo / total
+  end
+
   # Returns the arithmetic mean of the elements in the collection.
   # Raises `EmptyError` if the collection is empty.
   #
