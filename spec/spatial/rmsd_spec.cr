fe4bd7f96d337a8636022588d8937b227503f28d
Francisco Adasme <francisco.adasme@gmail.com>
2022-03-18 14:52:37 -0300

    Uncomment some rmsd specs

--- spec/spatial/rmsd_spec.cr
+++ spec/spatial/rmsd_spec.cr
@@ -31,9 +31,9 @@ describe Chem::Spatial do
       s = Array(Chem::Structure).read spec_file("E20_conformers.mol2")
       weights = s[0].atoms.map &.mass
       Chem::Spatial.rmsd(s[1], s[0], weights, minimize: true).should be_close 2.811033, 1e-6
-      # Chem::Spatial.rmsd(s[2], s[0], weights, minimize: true).should be_close 1.358219, 1e-6
-      # Chem::Spatial.rmsd(s[3], s[0], weights, minimize: true).should be_close 3.067433, 1e-6
-      # Chem::Spatial.rmsd(s[4], s[0], weights, minimize: true).should be_close 1.084173, 1e-6
+      Chem::Spatial.rmsd(s[2], s[0], weights, minimize: true).should be_close 1.358219, 1e-6
+      Chem::Spatial.rmsd(s[3], s[0], weights, minimize: true).should be_close 3.067433, 1e-6
+      Chem::Spatial.rmsd(s[4], s[0], weights, minimize: true).should be_close 1.084173, 1e-6
     end
   end
 end
