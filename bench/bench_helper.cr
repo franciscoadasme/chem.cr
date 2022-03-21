07a2d00203a06b4f20b4a299a0ed44beeeeeb177
Francisco Adasme <francisco.adasme@gmail.com>
2022-03-21 14:11:46 -0300

    Add benchmark utilities

--- bench/bench_helper.cr
+++ bench/bench_helper.cr
@@ -1,2 +1,24 @@
 require "benchmark"
 require "../src/chem"
+
+# Reports the best time (ms) of running the given block. The execution
+# is repeated *repeats* times such that the lowest time is returned to
+# avoid potential slow downs by external processes.
+def bench(label : String, repeats : Int = 10_000, & : ->) : Nil
+  {% if !flag?(:release) %}
+    puts "Warning: benchmarking without the `--release` flag won't yield useful results"
+  {% end %}
+  best_time = (0...repeats).min_of do
+    Time.measure do
+      yield
+    end
+  end
+  puts "#{label} took #{best_time.total_milliseconds} ms"
+end
+
+# Returns the path to the given data bench file.
+def data_file(filename : String) : String
+  path = File.join(__DIR__, "data", filename)
+  raise IO::Error.new("File not found: #{filename}") unless File.exists?(path)
+  path
+end
