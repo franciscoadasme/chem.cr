c842227bcdeeb9a0bd4c39dab8e4c970dc0d6486
Francisco Adasme <francisco.adasme@gmail.com>
2022-03-24 18:11:46 -0300

    Uncomment specs

--- spec/typing_spec.cr
+++ spec/typing_spec.cr
@@ -1,126 +1,126 @@
 require "./spec_helper"
 
-# describe Chem::AtomType do
-#   describe "#inspect" do
-#     it "returns a string representation" do
-#       Chem::AtomType.new("CA").inspect.should eq "<AtomType CA>"
-#       Chem::AtomType.new("NZ", formal_charge: 1).inspect.should eq "<AtomType NZ+>"
-#       Chem::AtomType.new("SG", valency: 1).inspect.should eq "<AtomType SG(1)>"
-#     end
-#   end
+describe Chem::AtomType do
+  describe "#inspect" do
+    it "returns a string representation" do
+      Chem::AtomType.new("CA").inspect.should eq "<AtomType CA>"
+      Chem::AtomType.new("NZ", formal_charge: 1).inspect.should eq "<AtomType NZ+>"
+      Chem::AtomType.new("SG", valency: 1).inspect.should eq "<AtomType SG(1)>"
+    end
+  end
 
-#   describe "#to_s" do
-#     it "returns atom name" do
-#       Chem::AtomType.new("CA").to_s.should eq "CA"
-#     end
+  describe "#to_s" do
+    it "returns atom name" do
+      Chem::AtomType.new("CA").to_s.should eq "CA"
+    end
 
-#     it "returns atom name plus charge sign when charge is not zero" do
-#       Chem::AtomType.new("NZ", formal_charge: 1).to_s.should eq "NZ+"
-#       Chem::AtomType.new("OE1", formal_charge: -1).to_s.should eq "OE1-"
-#       Chem::AtomType.new("NA", formal_charge: 2).to_s.should eq "NA+2"
-#       Chem::AtomType.new("UK", formal_charge: -5).to_s.should eq "UK-5"
-#     end
+    it "returns atom name plus charge sign when charge is not zero" do
+      Chem::AtomType.new("NZ", formal_charge: 1).to_s.should eq "NZ+"
+      Chem::AtomType.new("OE1", formal_charge: -1).to_s.should eq "OE1-"
+      Chem::AtomType.new("NA", formal_charge: 2).to_s.should eq "NA+2"
+      Chem::AtomType.new("UK", formal_charge: -5).to_s.should eq "UK-5"
+    end
 
-#     it "returns atom name plus valency when its not nominal" do
-#       Chem::AtomType.new("SG", valency: 1).to_s.should eq "SG(1)"
-#     end
-#   end
-# end
+    it "returns atom name plus valency when its not nominal" do
+      Chem::AtomType.new("SG", valency: 1).to_s.should eq "SG(1)"
+    end
+  end
+end
 
-# describe Chem::BondType do
-#   describe "#inspect" do
-#     it "returns a string representation" do
-#       Chem::BondType.new("CA", "CB").inspect.should eq "<BondType CA-CB>"
-#       Chem::BondType.new("C", "O", order: 2).inspect.should eq "<BondType C=O>"
-#       Chem::BondType.new("C", "N", order: 3).inspect.should eq "<BondType C#N>"
-#     end
-#   end
-# end
+describe Chem::BondType do
+  describe "#inspect" do
+    it "returns a string representation" do
+      Chem::BondType.new("CA", "CB").inspect.should eq "<BondType CA-CB>"
+      Chem::BondType.new("C", "O", order: 2).inspect.should eq "<BondType C=O>"
+      Chem::BondType.new("C", "N", order: 3).inspect.should eq "<BondType C#N>"
+    end
+  end
+end
 
-# describe Chem::ResidueType do
-#   Chem::ResidueType.register do
-#     description "Anything"
-#     name "LFG"
-#     structure do
-#       stem "N1+-C2-C3-O4-C5-C6"
-#       branch "C5=O7"
-#     end
-#     root "C5"
-#   end
+describe Chem::ResidueType do
+  Chem::ResidueType.register do
+    description "Anything"
+    name "LFG"
+    structure do
+      stem "N1+-C2-C3-O4-C5-C6"
+      branch "C5=O7"
+    end
+    root "C5"
+  end
 
-#   describe ".fetch" do
-#     it "returns a residue type by name" do
-#       residue_t = Chem::ResidueType.fetch("LFG")
-#       residue_t.should be_a Chem::ResidueType
-#       residue_t.description.should eq "Anything"
-#       residue_t.name.should eq "LFG"
-#     end
+  describe ".fetch" do
+    it "returns a residue type by name" do
+      residue_t = Chem::ResidueType.fetch("LFG")
+      residue_t.should be_a Chem::ResidueType
+      residue_t.description.should eq "Anything"
+      residue_t.name.should eq "LFG"
+    end
 
-#     it "raises if residue type does not exist" do
-#       expect_raises Chem::Error, "Unknown residue type ASD" do
-#         Chem::ResidueType.fetch("ASD")
-#       end
-#     end
+    it "raises if residue type does not exist" do
+      expect_raises Chem::Error, "Unknown residue type ASD" do
+        Chem::ResidueType.fetch("ASD")
+      end
+    end
 
-#     it "returns block's return value if residue type does not exist" do
-#       Chem::ResidueType.fetch("ASD") { nil }.should be_nil
-#     end
-#   end
+    it "returns block's return value if residue type does not exist" do
+      Chem::ResidueType.fetch("ASD") { nil }.should be_nil
+    end
+  end
 
-#   describe ".register" do
-#     it "creates a residue template with multiple names" do
-#       Chem::ResidueType.register do
-#         description "Anything"
-#         name "LXE"
-#         aliases "EGR"
-#         structure "C1"
-#       end
-#       Chem::ResidueType.fetch("LXE").should be Chem::ResidueType.fetch("EGR")
-#     end
+  describe ".register" do
+    it "creates a residue template with multiple names" do
+      Chem::ResidueType.register do
+        description "Anything"
+        name "LXE"
+        aliases "EGR"
+        structure "C1"
+      end
+      Chem::ResidueType.fetch("LXE").should be Chem::ResidueType.fetch("EGR")
+    end
 
-#     it "fails when the residue name already exists" do
-#       expect_raises Chem::Error, "LXE residue type already exists" do
-#         Chem::ResidueType.register do
-#           description "Anything"
-#           name "LXE"
-#           structure do
-#             stem "C1"
-#           end
-#           root "C1"
-#         end
-#       end
-#     end
-#   end
+    it "fails when the residue name already exists" do
+      expect_raises Chem::Error, "LXE residue type already exists" do
+        Chem::ResidueType.register do
+          description "Anything"
+          name "LXE"
+          structure do
+            stem "C1"
+          end
+          root "C1"
+        end
+      end
+    end
+  end
 
-#   describe "#inspect" do
-#     it "returns a string representation" do
-#       Chem::ResidueType.build do
-#         name "O2"
-#         description "Molecular oxygen"
-#         structure "O1=O2"
-#         root "O1"
-#       end.inspect.should eq "<ResidueType O2>"
+  describe "#inspect" do
+    it "returns a string representation" do
+      Chem::ResidueType.build do
+        name "O2"
+        description "Molecular oxygen"
+        structure "O1=O2"
+        root "O1"
+      end.inspect.should eq "<ResidueType O2>"
 
-#       Chem::ResidueType.build do
-#         name "HOH"
-#         kind :solvent
-#         description "Water"
-#         structure "O"
-#       end.inspect.should eq "<ResidueType HOH, solvent>"
+      Chem::ResidueType.build do
+        name "HOH"
+        kind :solvent
+        description "Water"
+        structure "O"
+      end.inspect.should eq "<ResidueType HOH, solvent>"
 
-#       Chem::ResidueType.build do
-#         description "Glycine"
-#         name "GLY"
-#         code 'G'
-#         kind :protein
-#         structure do
-#           backbone
-#           remove_atom "HA"
-#         end
-#       end.inspect.should eq "<ResidueType GLY(G), protein>"
-#     end
-#   end
-# end
+      Chem::ResidueType.build do
+        description "Glycine"
+        name "GLY"
+        code 'G'
+        kind :protein
+        structure do
+          backbone
+          remove_atom "HA"
+        end
+      end.inspect.should eq "<ResidueType GLY(G), protein>"
+    end
+  end
+end
 
 describe Chem::ResidueType::Builder do
   describe ".build" do
