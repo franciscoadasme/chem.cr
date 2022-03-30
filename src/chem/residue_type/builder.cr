5f27ec8345f4a65e96833b0a3cc88b56e0c7620a
Francisco Adasme <francisco.adasme@gmail.com>
2022-03-30 17:15:51 -0300

    Clean up ResidueType::Builder

--- src/chem/residue_type/builder.cr
+++ src/chem/residue_type/builder.cr
@@ -8,54 +8,19 @@ class Chem::ResidueType::Builder
   @symmetric_atom_groups = [] of Array(Tuple(String, String))
   @structure_parser : Parser?
 
-  private def add_hydrogens
-    use_name_suffix = @atom_types.any? &.suffix.to_i?.nil?
-    atom_types = use_name_suffix ? @atom_types : @atom_types.sort_by do |atom_t|
-      {atom_t.element.atomic_number * -1, atom_t.suffix.to_i}
-    end
-
-    h_i = 0
-    atom_types.each do |atom_type|
-      i = @atom_types.index! atom_type
-
-      # TODO: implement a sane valence model, e.g., smiles model in openbabel
-      bond_count = @bonds.sum { |bond| atom_type.in?(bond) ? bond.order : 0 }
-      if bond = @link_bond
-        bond_count += bond.order if atom_type.in?(bond)
-      end
-      bond_count += @implicit_bonds[atom_type.name]? || 0
-
-      valency = atom_type.element.valencies.find(&.>=(bond_count))
-      valency ||= atom_type.element.max_valency
-      valency += atom_type.element.ionic? ? -atom_type.formal_charge.abs : atom_type.formal_charge
-
-      h_count = valency - bond_count
-      if h_count >= 0
-        h_count.times do |j|
-          h_i += 1
-          name = use_name_suffix ? "H#{atom_type.suffix}#{j + 1 if h_count > 1}" : "H#{h_i}"
-          h_atom = AtomType.new(name, element: PeriodicTable::H)
-          @atom_types.insert i + 1 + j, h_atom
-          @bonds << BondType.new(atom_type, h_atom)
-        end
-      else
-        raise Error.new("Expected valency of #{atom_type.name} is #{valency}, \
-                         got #{bond_count}")
-      end
-    end
-  end
-
-  protected def build : ResidueType
+  def build : ResidueType
     raise Error.new("Missing residue name") if @names.empty?
     raise Error.new("Empty structure") unless parser = @structure_parser
-
     if @kind.protein? && {"C", "N", "CA"}.none? { |name| parser.atom_map[name]? }
       raise Error.new("Missing backbone atoms for #{@names.first}")
     end
+
+    # Set link bond if missing for known residue types
     @link_bond ||= case @kind
                    when .protein? then {"C", "N", 1}
                    end
 
+    # Count total of implicit bonds per atom
     implicit_bonds = Hash(String, Int32).new { 0 }
     parser.implicit_bonds.each do |bond|
       implicit_bonds[bond.lhs] += bond.order
@@ -68,9 +33,10 @@ class Chem::ResidueType::Builder
     parser.atom_map.each_value do |atom|
       element = atom.element || PeriodicTable[atom_name: atom.name]
 
+      # Calculate effective valence
       effective_valence = atom.explicit_hydrogens || 0
       effective_valence += atom.formal_charge * (element.valence_electrons >= 4 ? -1 : 1)
-      effective_valence += parser.bonds.sum do |bond|
+      effective_valence += parser.bonds.sum do |bond| # sum of bond orders
         atom.name.in?(bond.lhs, bond.rhs) ? bond.order : 0
       end
       effective_valence += implicit_bonds[atom.name]? || 0
@@ -78,6 +44,7 @@ class Chem::ResidueType::Builder
         effective_valence += bond[2] if atom.name.in?(bond)
       end
 
+      # Check that effective valence is correct
       target_valence = element.valence(effective_valence)
       if effective_valence > target_valence ||
          (atom.explicit_hydrogens && effective_valence != target_valence)
@@ -85,10 +52,12 @@ class Chem::ResidueType::Builder
                          got #{effective_valence}")
       end
 
+      # Create and register atom type
       atom_type = AtomType.new(atom.name, element, target_valence, atom.formal_charge)
       atom_types << atom_type
       atom_type_map[atom_type.name] = atom_type
 
+      # Add hydrogens (either explicit or implicit)
       suffix = atom_type.suffix.presence
       suffix = nil if suffix && suffix.size == 1 && suffix[0].ascii_number?
       h_count = atom.explicit_hydrogens || (target_valence - effective_valence)
@@ -107,18 +76,20 @@ class Chem::ResidueType::Builder
         bond.order)
     end
 
-    link_bond = @link_bond.try do |lhs, rhs, order|
-      BondType.new(atom_type_map[lhs], atom_type_map[rhs], order)
-    end
+    # Get root atom or sets it for known residue types if missing,
+    # otherwise raise
     root_atom = if atom_name = @root_atom
                   atom_type_map[atom_name]
                 elsif @kind.protein?
                   atom_type_map["CA"]
-                elsif atom_types.count(&.element.heavy?) == 1
+                elsif atom_types.count(&.element.heavy?) == 1 # one heavy atom
                   atom_types.first
                 else
                   raise Error.new("Missing root for residue type #{@names.first}")
                 end
+    link_bond = @link_bond.try do |lhs, rhs, order|
+      BondType.new(atom_type_map[lhs], atom_type_map[rhs], order)
+    end
 
     ResidueType.new @names.first, @code, @kind, @description,
       atom_types, bond_types, root_atom,
@@ -129,10 +100,8 @@ class Chem::ResidueType::Builder
     @code = char
   end
 
-  private def check_atom(name : String) : Parser::AtomRecord
-    if (parser = @structure_parser) && (atom = parser.atom_map[name]?)
-      atom
-    else
+  private def check_atom(name : String) : Nil
+    unless (parser = @structure_parser) && parser.atom_map[name]?
       raise Error.new("Unknown atom #{name}")
     end
   end
