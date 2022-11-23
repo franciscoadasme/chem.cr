2b0f42abef2d37bfd2a28a0b6606ee3936ffd943
Francisco Adasme <francisco.adasme@gmail.com>
2022-11-23 19:29:52 -0300

    format.psf: Fix bad column for extended variant

--- spec/formats/psf_spec.cr
+++ spec/formats/psf_spec.cr
@@ -39,4 +39,22 @@ describe Chem::PSF::Reader do
     top.dihedrals.size.should eq 40
     top.impropers.size.should eq 8
   end
+
+  it "parses PSF with extended format (#190)" do
+    top = Chem::Topology.from_psf spec_file("N4I.psf")
+    top.atoms.size.should eq 67
+    top.bonds.size.should eq 70
+    top.angles.size.should eq 124
+    top.dihedrals.size.should eq 187
+    top.impropers.size.should eq 20
+
+    atom = top.atoms[-1]
+    atom.serial.should eq 67
+    atom.residue.number.should eq 1
+    atom.residue.name.should eq "N4I"
+    atom.name.should eq "H28"
+    atom.typename.should eq "HALT"
+    atom.partial_charge.should eq 0.1555
+    atom.mass.should eq 1.008
+  end
 end
