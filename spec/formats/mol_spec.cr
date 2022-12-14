3b350b4c710dbb85da7c8ad27065620c976d0939
Francisco Adasme <francisco.adasme@gmail.com>
2022-12-14 18:07:51 -0300

    feat(format.mol): Add reading support for MDL MOL

--- spec/formats/mol_spec.cr
+++ spec/formats/mol_spec.cr
@@ -1,15 +1,35 @@
 require "../spec_helper"
 
 describe Chem::Mol::Reader do
-  it "parses a Mol file" do
-    structure = load_file "702.mol"
-    structure.source_file.should eq Path[spec_file("702.mol")].expand
+  it "parses a Mol V2000 file" do
+    path = Path[spec_file("702_v2000.mol")]
+    structure = Chem::Structure.read path
+    structure.source_file.should eq path.expand
     structure.title.should eq "702"
     structure.atoms.size.should eq 9
     structure.bonds.size.should eq 8
-    structure.atoms.map(&.formal_charge).should eq [
-      -2, 0, 0, 1, 0, -1, 0, 0, 0,
-    ]
+    structure.atoms.reject(&.formal_charge.zero?)
+      .to_h { |atom| {atom.serial, atom.formal_charge} }
+      .should eq({1 => -2, 4 => 1, 6 => -1})
+    structure.bonds.reject(&.single?)
+      .to_h { |bond| {bond.atoms.map(&.serial), bond.order.to_i} }
+      .should eq({ {1, 2} => 2 })
+    structure.atoms[2].mass.should eq 14
+  end
+
+  it "parses a Mol V3000 file" do
+    path = Path[spec_file("702_v3000.mol")]
+    structure = Chem::Structure.read path
+    structure.source_file.should eq path.expand
+    structure.title.should eq "702"
+    structure.atoms.size.should eq 9
+    structure.bonds.size.should eq 8
+    structure.atoms.reject(&.formal_charge.zero?)
+      .to_h { |atom| {atom.serial, atom.formal_charge} }
+      .should eq({1 => -2, 4 => 1, 6 => -1})
+    structure.bonds.reject(&.single?)
+      .to_h { |bond| {bond.atoms.map(&.serial), bond.order.to_i} }
+      .should eq({ {1, 2} => 2 })
     structure.atoms[2].mass.should eq 14
   rescue ex : Chem::ParseException
     puts ex.inspect_with_location
--- src/chem/formats/mol.cr
+++ src/chem/formats/mol.cr
@@ -1,22 +1,21 @@
+# This module provides support for reading MDL Mol files, including the
+# variants V2000 and V3000.
+#
+# Format specification found in the [CTFile
+# Formats](http://bit.ly/3WiuePn) document published by BIOVIA.
+#
+# WARNING: Basic support only. MDL valence model (implicit hydrogens),
+# connectivity information besides bonds, stereochemistry information
+# (e.g., chirality, 3D), advanced properties like Sgroup, reaction data,
+# etc. are unsupported/ignored. Therefore, **hydrogens are expected to be
+# defined explicitly**.
 @[Chem::RegisterFormat(ext: %w(.mol))]
 module Chem::Mol
-  enum Variant
-    V2000
-    V3000
-  end
-
   class Reader
     include FormatReader(Structure)
 
     def initialize(@io : IO, @sync_close : Bool = false)
       @pull = PullParser.new @io
-    end
-
-    private def check_v3000 : Nil
-      if {@pull.next_s?, @pull.next_s?} != {"M", "V30"}
-        @pull.rewind_line
-        @pull.error "Invalid V3000 line"
-      end
     end
 
     private def decode_entry : Structure
@@ -27,154 +26,75 @@ module Chem::Mol
       @pull.next_line # skip comment line
 
       variant = @pull.at(33, 6).parse("Invalid Mol variant %{token}") do |str|
-        Variant.parse? str.strip
+        case str.strip
+        when "V2000" then V2000
+        when "V3000" then V3000
+        end
       end
 
-      Structure.build(
+      structure = Structure.build(
         guess_bonds: false,
         guess_names: false,
         source_file: (file = @io).is_a?(File) ? file.path : nil,
         use_templates: false,
       ) do |builder|
         builder.title title
-        case variant
-        in .v2000?
-          parse_block_v2000(builder)
-        in .v3000?
-          @pull.next_line
-          parse_block_v3000(builder)
-        end
+        variant.parse @pull, builder
       end
+
+      if structure.atoms.all?(&.z.zero?) # non-3D
+        path = (io = @io).is_a?(File) ? "file #{io.path}" : "content"
+        Log.warn { "Detected non-3D molecule in MOL #{path}" }
+      end
+
+      structure
+    end
+  end
+
+  private module V2000
+    def self.parse(pull : PullParser, builder : Structure::Builder) : Nil
+      n_atoms = pull.at(0, 3).int "Invalid number of atoms %{token}"
+      n_bonds = pull.at(3, 3).int "Invalid number of bonds %{token}"
+      pull.next_line
+
+      n_atoms.times { parse_atom(pull, builder) }
+      n_bonds.times { parse_bond(pull, builder) }
+      parse_property_block(pull, builder)
     end
 
-    private def parse_block_v2000(builder : Structure::Builder) : Nil
-      n_atoms = @pull.at(0, 3).int "Invalid number of atoms %{token}"
-      n_bonds = @pull.at(3, 3).int "Invalid number of bonds %{token}"
-      @pull.next_line
-
-      n_atoms.times do
-        x = @pull.at(0, 10).float "Invalid X coordinate %{token}"
-        y = @pull.at(10, 10).float "Invalid Y coordinate %{token}"
-        z = @pull.at(20, 10).float "Invalid Z coordinate %{token}"
-        pos = Spatial::Vec3.new(x, y, z)
-        ele = @pull.at(31, 3).parse("Invalid element %{token}") do |str|
-          PeriodicTable[str.strip]?
-        end
-        mass = ele.mass + @pull.at?(34, 2)
-          .parse_if_present("Invalid mass difference %{token}", default: 0, &.to_i?)
-        chg = @pull.at?(36, 3)
-          .parse_if_present("Invalid formal charge %{token}", default: 0) do |str|
-            parse_formal_charge str
-          end
-        builder.atom ele, pos, formal_charge: chg, mass: mass
-        @pull.next_line
+    def self.parse_atom(pull : PullParser, builder : Structure::Builder) : Nil
+      x = pull.at(0, 10).float "Invalid X coordinate %{token}"
+      y = pull.at(10, 10).float "Invalid Y coordinate %{token}"
+      z = pull.at(20, 10).float "Invalid Z coordinate %{token}"
+      pos = Spatial::Vec3.new(x, y, z)
+      ele = pull.at(31, 3).parse("Invalid element %{token}") do |str|
+        PeriodicTable[str.strip]?
       end
-
-      n_bonds.times do
-        i = @pull.at(0, 3).int("Invalid atom index %{token}")
-        j = @pull.at(3, 3).int("Invalid atom index %{token}")
-        @pull.at(6, 3).parse("Invalid bond type %{token}") do |str|
-          case bond_type = str.strip
-          when "1", "2", "3"
-            builder.bond i, j, BondOrder.from_value(bond_type.to_i)
-          when "4"
-            builder.bond i, j, aromatic: true
-          end
+      mass = ele.mass + pull.at?(34, 2)
+        .parse_if_present("Invalid mass difference %{token}", default: 0, &.to_i?)
+      chg = pull.at?(36, 3)
+        .parse_if_present("Invalid formal charge %{token}", default: 0) do |str|
+          parse_formal_charge str
         end
-        @pull.next_line
-      end
-
-      @pull.each_line do
-        next unless @pull.at?(0, 3).str? == "M  "
-
-        property_name = @pull.at?(3, 3).str?
-        break if property_name == "END"
-        next unless property_name.in?("CHG", "ISO")
-
-        n_entries = @pull.at(6, 3).int "Invalid number of entries"
-        @pull.error("Number of entries expected within 1 to 8") unless n_entries.in?(1..8)
-        n_entries.times do |i|
-          col = 9 + i * 8
-          atom_i = @pull.at(col, 4).int "Invalid atom index"
-          atom = builder.atom?(atom_i) || @pull.error("Atom index #{atom_i} out of range")
-          @pull.at(col + 4, 4)
-          case property_name
-          when "CHG"
-            atom.formal_charge = @pull.int "Invalid formal charge %{token}"
-          when "ISO"
-            atom.mass = @pull.float "Invalid isotope mass %{token}"
-          end
-        end
-      end
+      builder.atom ele, pos, formal_charge: chg, mass: mass
+      pull.next_line
     end
 
-    # TODO: add support for continuation line '-'
-    private def parse_block_v3000(builder : Structure::Builder) : Nil
-      @pull.each_line do
-        case {@pull.next_s?, @pull.next_s?}
-        when {"M", "V30"}
-          next unless @pull.next_s == "BEGIN"
-        when {"M", "END"}
-          @pull.next_line
-          break
-        else # line should not be consumed
-          @pull.rewind_line
-          break
-        end
-
-        n_atoms = n_bonds = 0
-        case @pull.next_s.tap { @pull.next_line }
-        when "CTAB"
-          check_v3000
-          if @pull.next_s? != "COUNTS"
-            @pull.rewind_line
-            @pull.error "Expected COUNTS line after BEGIN CTAB"
-          end
-          n_atoms = @pull.next_i "Invalid number of atoms %{token}"
-          n_bonds = @pull.next_i "Invalid number of bonds %{token}"
-        when "ATOM"
-          n_atoms.times do
-            check_v3000
-            serial = @pull.next_i "Invalid atom serial %{token}"
-            ele = @pull.parse_next("Invalid element %{token}") do |str|
-              PeriodicTable[str]?
-            end
-            x = @pull.next_f "Invalid X coordinate %{token}"
-            y = @pull.next_f "Invalid Y coordinate %{token}"
-            z = @pull.next_f "Invalid Z coordinate %{token}"
-            pos = Spatial::Vec3.new(x, y, z)
-            @pull.next_token # skip aamap
-
-            chg = 0
-            mass = ele.mass
-            while str = @pull.next_s?
-              name, _, str = str.partition("=")
-              case name
-              when "CHG"
-                chg = str.to_i? || @pull.error("Invalid formal charge %{token}")
-              when "MASS"
-                mass = str.to_f? || @pull.error("Invalid isotope mass %{token}")
-              end
-            end
-
-            builder.atom ele, pos, formal_charge: chg, mass: mass
-            @pull.next_line
-          end
-
-          if @pull.line.split == %w(M V30 END ATOM)
-            @pull.next_line
-          else
-            @pull.error "Expected END ATOM line after atoms"
-          end
-        when "BOND"
-          n_bonds.times do
-            check_v3000
-          end
+    def self.parse_bond(pull : PullParser, builder : Structure::Builder) : Nil
+      i = pull.at(0, 3).int("Invalid atom index %{token}")
+      j = pull.at(3, 3).int("Invalid atom index %{token}")
+      pull.at(6, 3).parse("Invalid bond type %{token}") do |str|
+        case bond_type = str.strip
+        when "1", "2", "3"
+          builder.bond i, j, BondOrder.from_value(bond_type.to_i)
+        when "4"
+          builder.bond i, j, aromatic: true
         end
       end
+      pull.next_line
     end
 
-    private def parse_formal_charge(str) : Int32?
+    def self.parse_formal_charge(str) : Int32?
       case str.strip
       when "0" then 0
       when "3" then 1
@@ -184,6 +104,144 @@ module Chem::Mol
       when "6" then -2
       when "7" then -3
       end
+    end
+
+    def self.parse_property_block(pull : PullParser, builder : Structure::Builder) : Nil
+      pull.each_line do
+        next unless pull.at?(0, 3).str? == "M  "
+
+        property_name = pull.at?(3, 3).str?
+        break if property_name == "END"
+        next unless property_name.in?("CHG", "ISO")
+
+        n_entries = pull.at(6, 3).int "Invalid number of entries"
+        pull.error("Number of entries expected within 1 to 8") unless n_entries.in?(1..8)
+        n_entries.times do |i|
+          col = 9 + i * 8
+          atom_i = pull.at(col, 4).int "Invalid atom index"
+          atom = builder.atom?(atom_i) || pull.error("Atom index #{atom_i} out of range")
+          pull.at(col + 4, 4)
+          case property_name
+          when "CHG"
+            atom.formal_charge = pull.int "Invalid formal charge %{token}"
+          when "ISO"
+            atom.mass = pull.float "Invalid isotope mass %{token}"
+          end
+        end
+      end
+    end
+  end
+
+  private module V3000
+    def self.check_block_close(pull : PullParser, name : String) : Nil
+      check_entry pull, {"END", name}, "Expected END #{name} to close #{name} block"
+      pull.next_line
+    end
+
+    def self.check_entry(
+      pull : PullParser,
+      tokens : Tuple,
+      message : String = "Expected #{name} entry at %{loc_with_file}"
+    ) : Nil
+      check_entry_tag(pull)
+      if tokens.map { pull.next_s? } != tokens
+        pull.rewind_line
+        pull.error(message)
+      end
+    end
+
+    def self.check_entry_tag(
+      pull : PullParser,
+      message : String = "Invalid V3000 entry at %{loc_with_file}"
+    ) : Nil
+      if {pull.next_s?, pull.next_s?} != {"M", "V30"}
+        pull.rewind_line
+        pull.error(message)
+      end
+    end
+
+    def self.next_block(pull : PullParser) : String?
+      pull.each_line do
+        case {pull.next_s?, pull.next_s?}
+        when {"M", "V30"}
+          return pull.next_s.tap { pull.next_line } if pull.next_s == "BEGIN"
+        when {"M", "END"}
+          pull.next_line
+          break
+        else # line should not be consumed
+          pull.rewind_line
+          break
+        end
+      end
+    end
+
+    def self.parse(pull : PullParser, builder : Structure::Builder) : Nil
+      pull.next_line # skips V2000 compatibility line
+      n_atoms = n_bonds = 0
+      while name = next_block(pull)
+        case name
+        when "CTAB"
+          check_entry(pull, {"COUNTS"}, "Expected COUNTS entry after BEGIN CTAB")
+          n_atoms = pull.next_i "Invalid number of atoms %{token}"
+          n_bonds = pull.next_i "Invalid number of bonds %{token}"
+          pull.next_line
+        when "ATOM"
+          n_atoms.times { parse_atom(pull, builder) }
+          check_block_close pull, "ATOM"
+        when "BOND"
+          n_bonds.times { parse_bond(pull, builder) }
+          check_block_close pull, "BOND"
+        end
+      end
+    end
+
+    def self.parse_atom(pull : PullParser, builder : Structure::Builder) : Nil
+      check_entry_tag(pull)
+      serial = pull.next_i "Invalid atom serial %{token}"
+      ele = pull.parse_next("Invalid element %{token}") do |str|
+        PeriodicTable[str]?
+      end
+      x = pull.next_f "Invalid X coordinate %{token}"
+      y = pull.next_f "Invalid Y coordinate %{token}"
+      z = pull.next_f "Invalid Z coordinate %{token}"
+      pos = Spatial::Vec3.new(x, y, z)
+      pull.next_token # skip aamap
+
+      chg = 0
+      mass = ele.mass
+      while str = pull.next_s?
+        name, _, str = str.partition("=")
+        case name
+        when "CHG"
+          chg = str.to_i? || pull.error("Invalid formal charge %{token}")
+        when "MASS"
+          mass = str.to_f? || pull.error("Invalid isotope mass %{token}")
+        end
+      end
+
+      builder.atom(ele, pos, formal_charge: chg, mass: mass)
+      pull.next_line
+    end
+
+    def self.parse_bond(pull : PullParser, builder : Structure::Builder) : Nil
+      check_entry_tag(pull)
+      pull.next_token # ignore index
+
+      aromatic = false
+      bond_order = pull.parse_next("Invalid bond type %{token}") do |str|
+        case bond_type = str
+        when "1", "2", "3"
+          BondOrder.from_value(bond_type.to_i)
+        when "4"
+          aromatic = true
+          BondOrder::Single
+        end
+      end
+
+      i = pull.next_i("Invalid atom index %{token}")
+      j = pull.next_i("Invalid atom index %{token}")
+      builder.bond i, j, bond_order, aromatic: aromatic
+      pull.next_line
     end
   end
 end
