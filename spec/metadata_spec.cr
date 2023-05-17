c3be7df04e7a7a0c5e964c468fd98cf732a57c3b
Francisco Adasme <francisco.adasme@gmail.com>
2023-05-17 13:21:10 -0400

    feat: Fully implement Metadata

--- spec/metadata_spec.cr
+++ spec/metadata_spec.cr
@@ -1,11 +1,16 @@
 require "./spec_helper"
 
 describe Chem::Metadata::Any do
-  describe "#raw" do
-    it "returns the enclosed value" do
-      any = Chem::Metadata::Any.new("1")
-      any.raw.should be_a String
-      any.raw.should eq "1"
+  describe "#==" do
+    it "compares the enclosed values" do
+      Chem::Metadata::Any.new("123").should eq Chem::Metadata::Any.new("123")
+      Chem::Metadata::Any.new("123").should_not eq Chem::Metadata::Any.new(123)
+    end
+
+    it "compares the enclosed value with any value" do
+      Chem::Metadata::Any.new("123").should eq "123"
+      Chem::Metadata::Any.new("123").should_not eq 123
+      Chem::Metadata::Any.new("123").should_not eq [] of Int32
     end
   end
 
@@ -97,7 +102,16 @@ describe Chem::Metadata::Any do
 
   describe "#inspect" do
     it "inspect the object" do
-      Chem::Metadata::Any.new(Math::PI).inspect.should eq "Chem::Metadata::Any(#{Math::PI})"
+      Chem::Metadata::Any.new(Math::PI).inspect.should eq "#{Chem::Metadata::Any}(#{Math::PI})"
+      Chem::Metadata::Any.new("foo").inspect.should eq %(#{Chem::Metadata::Any}("foo"))
+    end
+  end
+
+  describe "#raw" do
+    it "returns the enclosed value" do
+      any = Chem::Metadata::Any.new("1")
+      any.raw.should be_a String
+      any.raw.should eq "1"
     end
   end
 
@@ -106,4 +120,274 @@ describe Chem::Metadata::Any do
       Chem::Metadata::Any.new(Math::PI).to_s.should eq Math::PI.to_s
     end
   end
+end
+
+describe Chem::Metadata do
+  describe "#[]" do
+    it "returns the value for a key" do
+      fake_metadata["str"].should be_a Chem::Metadata::Any
+      fake_metadata["str"].should eq "Foo"
+    end
+
+    it "raises if key does not exist" do
+      expect_raises(KeyError) { fake_metadata["foo"] }
+    end
+  end
+
+  describe "#[]=" do
+    it "sets a value for key" do
+      metadata = fake_metadata
+      metadata["foo"] = "bar"
+      metadata["foo"].should be_a Chem::Metadata::Any
+      metadata["foo"].should eq "bar"
+    end
+  end
+
+  describe "#[]?" do
+    it "returns the value for a key" do
+      fake_metadata["int"]?.should be_a Chem::Metadata::Any
+      fake_metadata["int"]?.should eq 123
+    end
+
+    it "returns nil key does not exist" do
+      fake_metadata["foo"]?.should be_nil
+    end
+  end
+
+  describe "#each" do
+    it "yields each key-value pair" do
+      entries = [] of {String, Chem::Metadata::Any}
+      fake_metadata.each { |kv| entries << kv }
+      entries.should eq fake_metadata.to_a
+    end
+
+    it "returns an iterator of each key-value pair" do
+      fake_metadata.each.to_a.should eq fake_metadata.to_a
+    end
+  end
+
+  describe "#clear" do
+    it "empties the metadata" do
+      fake_metadata.clear.should be_empty
+    end
+  end
+
+  describe "#delete" do
+    it "removes a key-value pair" do
+      metadata = fake_metadata
+      metadata.delete("float").should eq 1.234
+      metadata.keys.should eq %w(str int bool)
+    end
+
+    it "yields if key does not exist" do
+      fake_metadata.delete("foo") { |key| key.upcase }.should eq "FOO"
+    end
+  end
+
+  describe "#each" do
+    it "yields each key-value pair" do
+      entries = [] of {String, Chem::Metadata::Any}
+      fake_metadata.each { |kv| entries << kv }
+      entries.should eq fake_metadata.to_a
+    end
+
+    it "returns an iterator of each key-value pair" do
+      fake_metadata.each.to_a.should eq fake_metadata.to_a
+    end
+  end
+
+  describe "#each_key" do
+    it "yields each key" do
+      keys = [] of String
+      fake_metadata.each_key { |key| keys << key }
+      keys.should eq fake_metadata.keys
+    end
+
+    it "returns an iterator of each key" do
+      fake_metadata.each_key.to_a.should eq fake_metadata.keys
+    end
+  end
+
+  describe "#each_value" do
+    it "yields each value" do
+      values = [] of Chem::Metadata::Any
+      fake_metadata.each_value { |value| values << value }
+      values.should eq fake_metadata.values
+    end
+
+    it "returns an iterator of each value" do
+      fake_metadata.each_value.to_a.should eq fake_metadata.values
+    end
+  end
+
+  describe "#empty?" do
+    it "returns true if empty" do
+      Chem::Metadata.new.empty?.should be_true
+    end
+
+    it "returns false if not empty" do
+      fake_metadata.empty?.should be_false
+    end
+  end
+
+  describe "#fetch" do
+    it "returns the value for key" do
+      fake_metadata.fetch("str", Math::PI).should eq "Foo"
+    end
+
+    it "returns the default value if key does not exist" do
+      fake_metadata.fetch("foo", Math::PI).should eq Math::PI
+    end
+
+    it "returns the returned value by the block if key does not exist" do
+      fake_metadata.fetch("foo") { |key| key.upcase }.should eq "FOO"
+    end
+  end
+
+  describe "#has_key?" do
+    it "returns true if key exists" do
+      fake_metadata.has_key?("str").should be_true
+    end
+
+    it "returns false if key does not exist" do
+      fake_metadata.has_key?("foo").should be_false
+    end
+  end
+
+  describe "#has_value?" do
+    it "returns true if value exists" do
+      fake_metadata.has_value?("Foo").should be_true
+    end
+
+    it "returns false if value does not exist" do
+      fake_metadata.has_value?(Math::PI).should be_false
+    end
+  end
+
+  describe "#inspect" do
+    it "returns a string representation" do
+      fake_metadata.inspect.should eq "#{Chem::Metadata}#{fake_metadata}"
+    end
+  end
+
+  describe "#key_for" do
+    it "returns the key for the given value" do
+      fake_metadata.key_for("Foo").should eq "str"
+    end
+
+    it "returns the returned value by the block if key does not exist" do
+      fake_metadata.key_for("Bar") { |value| value.upcase }.should eq "BAR"
+    end
+
+    it "raises if value does not exist" do
+      expect_raises(KeyError) { fake_metadata.key_for(Math::PI) }
+    end
+  end
+
+  describe "#key_for?" do
+    it "returns the key for the given value" do
+      fake_metadata.key_for?("Foo").should eq "str"
+    end
+
+    it "returns nil if value does not exist" do
+      fake_metadata.key_for?(Math::PI).should be_nil
+    end
+  end
+
+  describe "#keys" do
+    it "returns the keys" do
+      fake_metadata.keys.should eq %w(str int float bool)
+    end
+  end
+
+  describe "#reject!" do
+    it "deletes entries when block is truthy" do
+      metadata = fake_metadata
+      metadata.reject! { |key, value| value.as_i? }
+      metadata.keys.should eq %w(str float bool)
+    end
+
+    it "deletes the given keys" do
+      metadata = fake_metadata
+      metadata.reject! %w(str bool)
+      metadata.keys.should eq %w(int float)
+    end
+  end
+
+  describe "#select!" do
+    it "deletes entries when block is falsy" do
+      metadata = fake_metadata
+      metadata.select! { |key, value| value.as_i? }
+      metadata.keys.should eq %w(int)
+    end
+
+    it "selects the given keys" do
+      metadata = fake_metadata
+      metadata.select! %w(str bool)
+      metadata.keys.should eq %w(str bool)
+    end
+  end
+
+  describe "#size" do
+    it "returns the number of key-value pairs" do
+      fake_metadata.size.should eq 4
+      fake_metadata.clear.size.should eq 0
+    end
+  end
+
+  describe "#to_a" do
+    it "returns the key-value pairs" do
+      fake_metadata.to_a.should eq [
+        {"str", Chem::Metadata::Any.new("Foo")},
+        {"int", Chem::Metadata::Any.new(123)},
+        {"float", Chem::Metadata::Any.new(1.234)},
+        {"bool", Chem::Metadata::Any.new(true)},
+      ]
+    end
+  end
+
+  describe "#to_s" do
+    it "returns the string representation" do
+      fake_metadata.to_s.should eq \
+        %({"str" => "Foo", "int" => 123, "float" => 1.234, "bool" => true})
+    end
+  end
+
+  describe "#update" do
+    it "updates a value" do
+      metadata = fake_metadata
+      metadata.update("int", &.as_i.*(2))
+      metadata["int"].should eq 246
+    end
+
+    it "raises if key does not exist" do
+      expect_raises(KeyError) { fake_metadata.update("foo") { 2 } }
+    end
+  end
+
+  describe "#values" do
+    it "returns the values" do
+      fake_metadata.values.should eq ["Foo", 123, 1.234, true]
+    end
+  end
+
+  describe "#values_at" do
+    it "returns the values" do
+      fake_metadata.values_at("str", "float").should eq({"Foo", 1.234})
+      fake_metadata.values_at(["str", "float"]).should eq ["Foo", 1.234]
+    end
+
+    it "raises if a key does not exist" do
+      expect_raises(KeyError) { fake_metadata.values_at("foo") }
+    end
+  end
+end
+
+private def fake_metadata : Chem::Metadata
+  metadata = Chem::Metadata.new
+  metadata["str"] = "Foo"
+  metadata["int"] = 123
+  metadata["float"] = 1.234
+  metadata["bool"] = true
+  metadata
 end
--- src/chem/metadata.cr
+++ src/chem/metadata.cr
@@ -1,5 +1,33 @@
+# A `Metadata` is a hash-like container that holds metadata about an
+# object. It maps a property name (as string) to a primitive value
+# (integer, float, string, or bool). Most of the functionality mirrors
+# that of a `Hash`, where some specific methods like `Hash#dig` or
+# default values are excluded.
+#
+# Values are stored as `Any` instances, which a thin wrapper for a
+# dynamically-typed primitive value and it offers convenient `#as_*`
+# cast methods.
+#
+# ### Examples
+#
+# ```
+# metadata = Chem::Metadata.new
+#
+# # only four data types are supported
+# metadata["str"] = "Foo"
+# metadata["int"] = 123
+# metadata["float"] = 1.234
+# metadata["bool"] = true
+# # metadata["x"] = /[a-z]/ # fails to compile
+#
+# # values are stored as `Any` instances
+# metadata["str"]             # => Chem::Metadata::Type("Foo")
+# metadata["str"].as_s.upcase # => "FOO"
+# metadata["str"].as_i?       # => nil
+# metadata["str"].as_i        # raises TypeCastError
+# ```
 class Chem::Metadata
-  # Union of possible metadata types.
+  # Union of possible metadata value types.
   alias ValueType = Int32 | Float64 | String | Bool
 
   include Enumerable({String, ValueType})
@@ -8,16 +36,29 @@ class Chem::Metadata
   # `Any` wraps a value that can be any of the possible metadata
   # types (`ValueType`). It provides convenient `#as_*` cast methods.
   struct Any
+    # Returns the enclosed value.
     getter raw : ValueType
 
     # Creates a new `Any` instance by enclosing the given value.
     def initialize(@raw : ValueType)
     end
 
+    # Returns `true` if the enclosed values are equal, else `false`.
+    #
+    # ```
+    # Chem::Metadata::Any.new("123") == Chem::Metadata::Any.new("123") # => true
+    # Chem::Metadata::Any.new("123") == Chem::Metadata::Any.new(123)   # => false
+    # ```
     def ==(rhs : self) : Bool
       @raw == rhs.raw
     end
 
+    # Returns `true` if the enclosed value is equal to *rhs*, else `false`.
+    #
+    # ```
+    # Chem::Metadata::Any.new("123") == "123" # => true
+    # Chem::Metadata::Any.new("123") == 123   # => false
+    # ```
     def ==(rhs) : Bool
       @raw == rhs
     end
@@ -81,7 +122,9 @@ class Chem::Metadata
     end
 
     def inspect(io : IO) : Nil
-      io << self.class.name << '(' << @raw << ')'
+      io << self.class.name << '('
+      @raw.inspect io
+      io << ')'
     end
 
     def to_s(io : IO) : Nil
@@ -110,19 +153,10 @@ class Chem::Metadata
     @data[key]?
   end
 
-  def each(& : {String, Type} ->) : Nil
-    @data.each do |keyvalue|
-      yield keyvalue
-    end
-  end
-
-  def each : Iterator({String, Type})
-    @data.each
-  end
-
   # Empties the `Metadata` and returns it.
   def clear : self
     @data.clear
+    self
   end
 
   # Deletes the key-value pair and returns the value. Returns `nil` if
@@ -134,8 +168,18 @@ class Chem::Metadata
   # Deletes the key-value pair and returns the value. Yields *key* and
   # returns the value returned by the given block if *key* does not
   # exist.
-  def delete(key : String, & : String ->) : Any?
+  def delete(key : String, & : String -> T) : Any | T forall T
     @data.delete(key) { |key| yield key }
+  end
+
+  def each(& : {String, Any} ->) : Nil
+    @data.each do |keyvalue|
+      yield keyvalue
+    end
+  end
+
+  def each : Iterator({String, Any})
+    @data.each
   end
 
   # Yields each key to the given block.
@@ -168,9 +212,141 @@ class Chem::Metadata
     @data.empty?
   end
 
-  # Returns `true` when the metadata contains no key-value pairs, else
+  # Returns the value if the given key exists, else *default*.
+  def fetch(key : String, default : T) : Any | T forall T
+    @data.fetch(key, default)
+  end
+
+  # Returns the value if the given key exists, else the value returned
+  # by the given block invoked with *key*.
+  def fetch(key : String, & : String -> T) : Any | T forall T
+    @data.fetch(key) { |key| yield key }
+  end
+
+  # Returns `true` if *key* exists, else `false`.
+  def has_key?(key : String) : Bool
+    @data.has_key?(key)
+  end
+
+  # Returns `true` if any key is associated with *value*, else
   # `false`.
-  def fetch? : Bool
-    @data.empty?
+  def has_value?(value : ValueType) : Bool
+    @data.has_value?(value)
+  end
+
+  def inspect(io : IO) : Nil
+    io << self.class.name
+    to_s io
+  end
+
+  # Returns a key with the given value. Raises `KeyError` if no key is
+  # associated with *value*.
+  def key_for(value : ValueType) : String
+    @data.key_for(value)
+  end
+
+  {% for type in ValueType.union_types %}
+    # Returns a key with the given value. Yields *value* to the given
+    # block and returns the returned value if no key is associated with
+    # *value*.
+    def key_for(value : {{type}}, & : {{type}} -> T) : String | T forall T
+      @data.key_for(value) { |value| yield value }
+    end
+  {% end %}
+
+  # Returns a key with the given value, or `nil` if no key is
+  # associated with *value*.
+  def key_for?(value : ValueType) : String?
+    @data.key_for?(value)
+  end
+
+  # Returns a new `Array` with all the keys.
+  def keys : Array(String)
+    @data.keys
+  end
+
+  # Deletes the entries for which the given block is truthy.
+  def reject!(& : String, Any ->) : self
+    @data.reject! { |key, value| yield key, value }
+    self
+  end
+
+  # Deletes the entries for the given keys.
+  def reject!(keys : Enumerable(String)) : self
+    @data.reject!(keys)
+    self
+  end
+
+  # :ditto:
+  def reject!(*keys : String) : self
+    @data.reject!(*keys)
+    self
+  end
+
+  # Deletes every entry except for which the given block is falsey.
+  def select!(& : String, Any ->) : self
+    @data.select! { |key, value| yield key, value }
+    self
+  end
+
+  # Deletes every entry except for the given keys.
+  def select!(keys : Enumerable(String)) : self
+    @data.select!(keys)
+    self
+  end
+
+  # :ditto:
+  def select!(*keys : String) : self
+    @data.select!(*keys)
+    self
+  end
+
+  # Returns the number of key-value pairs.
+  def size : Int32
+    @data.size
+  end
+
+  # Returns an array containing key-value pairs as tuples.
+  def to_a : Array({String, Any})
+    @data.to_a
+  end
+
+  def to_s(io : IO) : Nil
+    io << '{'
+    each_with_index do |(key, value), i|
+      key.inspect io
+      io << " => "
+      value.raw.inspect io
+      io << ", " if i < size - 1
+    end
+    io << '}'
+  end
+
+  # Yields the value for the given key and updates it with the value
+  # returned by the given block. Raises `KeyError` if *key* does not
+  # exist.
+  #
+  # It returns the value used as input for the given block (i.e., the
+  # old value).
+  def update(key : String, & : Any -> ValueType) : Any
+    @data.update(key) { |value| Any.new(yield value) }
+  end
+
+  # Returns an array containing the values.
+  def values : Array(Any)
+    @data.values
+  end
+
+  # Returns the values for the given keys. Raises `KeyError` if a key
+  # does not exist.
+  #
+  # Values are returned in the same order of the keys.
+  def values_at(keys : Enumerable(String)) : Array(Any)
+    keys.map { |key| @data[key] }
+  end
+
+  # :ditto:
+  def values_at(*keys : String)
+    @data.values_at(*keys)
   end
 end
