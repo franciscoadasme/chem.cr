45e40e6ee819067b137fcfc4f099c3bca213a4db
Francisco Adasme <francisco.adasme@gmail.com>
2023-05-26 20:08:50 -0400

    feat(io): Add support for extended XYZ (#105)

--- spec/formats/xyz_spec.cr
+++ spec/formats/xyz_spec.cr
@@ -63,6 +63,36 @@ describe Chem::XYZ::Reader do
       Array(Chem::Structure).from_xyz spec_file("coo.trj.xyz"), indexes: [5]
     end
   end
+
+  it "parses extended XYZ (#105)" do
+    structure = Chem::Structure.from_xyz spec_file("extended.xyz")
+    structure.title.should eq "Cubic Si"
+    structure.cell.size.should eq Chem::Spatial::Size3[5.44, 5.44, 5.44]
+    structure.cell.cubic?.should be_true
+    structure.metadata.keys.sort!.should eq %w(
+      converged dipole energy pressure step time wrap)
+    structure.metadata["converged"].should be_true
+    structure.metadata["dipole"].should eq [0.234, 1.234, 10.234]
+    structure.metadata["energy"].should eq -123.5e-3
+    structure.metadata["pressure"].should eq [1.23, 1.254, 0.923]
+    structure.metadata["step"].should eq 515050
+    structure.metadata["time"].should eq 1030.1
+    structure.metadata["wrap"].should be_false
+    structure.chains.map(&.id).should eq ['A', 'B', 'C']
+    structure.chains.map(&.residues.size).should eq [4, 3, 1]
+    structure.residues.map(&.name).should eq ["SIL"] * 8
+    structure.residues.map(&.number).should eq [1, 2, 3, 4, 1, 2, 3, 1]
+    structure.atoms.size.should eq 8
+    structure.atoms.map(&.element).uniq!.should eq [Chem::PeriodicTable::Si]
+    structure.atoms[-2].coords.should eq vec3(0, 2.72, 2.72)
+    structure.atoms.map(&.formal_charge).should eq [0, 0, 0, 2, 0, 0, 1, 0]
+    structure.atoms.count(&.partial_charge.zero?).should eq 8
+    structure.atoms.each_with_index do |atom, i|
+      atom.metadata.keys.sort!.should eq %w(dd label)
+      atom.metadata["dd"].should eq [0.1, 0.2, 0.3]
+      atom.metadata["label"].should eq "si#{i + 1}"
+    end
+  end
 end
 
 describe Chem::XYZ::Writer do
@@ -77,9 +107,10 @@ describe Chem::XYZ::Writer do
     structure.chains[0].to_xyz.should eq <<-EOS
       3
 
-      C          0.00000        0.00000        0.00000
-      O          0.00000        0.00000        1.15908
-      O          0.00000        0.00000       -1.15908\n
+      C     0.000    0.000    0.000
+      O     0.000    0.000    1.159
+      O     0.000    0.000   -1.159
+
       EOS
   end
 
@@ -103,19 +134,39 @@ describe Chem::XYZ::Writer do
     io.to_s.should eq <<-EOS
       3
       COO- Step 1
-      C          1.00000        0.00000        0.00000
-      O          2.00000        0.00000        0.00000
-      O          3.00000        0.00000        0.00000
+      C     1.000    0.000    0.000
+      O     2.000    0.000    0.000
+      O     3.000    0.000    0.000
       3
       COO- Step 2
-      C          2.00000        0.00000        0.00000
-      O          4.00000        0.00000        0.00000
-      O          6.00000        0.00000        0.00000
+      C     2.000    0.000    0.000
+      O     4.000    0.000    0.000
+      O     6.000    0.000    0.000
       3
       COO- Step 3
-      C          6.00000        0.00000        0.00000
-      O         12.00000        0.00000        0.00000
-      O         18.00000        0.00000        0.00000
+      C     6.000    0.000    0.000
+      O    12.000    0.000    0.000
+      O    18.000    0.000    0.000
+
+      EOS
+  end
+
+  it "writes extended" do
+    structure = Chem::Structure.from_xyz spec_file("extended.xyz")
+    structure.to_xyz(
+      extended: true,
+      fields: %w(constraint chain resid resname charge dd label)
+    ).should eq <<-EOS
+      8
+      Title="Cubic Si" Properties=species:S:1:pos:R:3:constraint:L:3:chain:S:1:resid:I:1:resname:S:1:charge:I:1:dd:R:3:label:S:1 Lattice=[[5.44, 0.0, 0.0], [0.0, 5.44, 0.0], [0.0, 0.0, 5.44]] Energy=-0.1235 Dipole=[0.234, 1.234, 10.234] Pressure=[1.23, 1.254, 0.923] Time=1030.1 Step=515050 Converged=true Wrap=false
+      Si    0.000    0.000    0.000 T F F A    1 SIL   0      0.1     0.2     0.3 si1
+      Si    1.360    1.360    1.360 T F F A    2 SIL   0      0.1     0.2     0.3 si2
+      Si    2.720    2.720    0.000 T F F A    3 SIL   0      0.1     0.2     0.3 si3
+      Si    4.080    4.080    1.360 T F F A    4 SIL   2      0.1     0.2     0.3 si4
+      Si    2.720    0.000    2.720 T F F B    1 SIL   0      0.1     0.2     0.3 si5
+      Si    4.080    1.360    4.080 T F F B    2 SIL   0      0.1     0.2     0.3 si6
+      Si    0.000    2.720    2.720 T T T B    3 SIL   1      0.1     0.2     0.3 si7
+      Si    1.360    4.080    4.080 T T T C    1 SIL   0      0.1     0.2     0.3 si8
 
       EOS
   end
--- src/chem/formats/xyz.cr
+++ src/chem/formats/xyz.cr
@@ -15,25 +15,113 @@ module Chem::XYZ
 
     protected def decode_entry : Structure
       raise IO::EOFError.new if @pull.eof?
-      Structure.build(
+
+      n_atoms = @pull.next_i
+
+      @pull.consume_line
+      ext_parser = ConfigurationParser.new(@pull)
+      ext_parser.parse
+
+      structure = Structure.build(
         guess_bonds: @guess_bonds,
         guess_names: @guess_names,
         source_file: (file = @io).is_a?(File) ? file.path : nil,
         use_templates: false,
       ) do |builder|
-        n_atoms = @pull.next_i
-        @pull.consume_line
-        metadata, columns = ExtendedParser.new(@pull).parse
-        builder.title columns.empty? ? @pull.line.strip : (metadata["title"].as_s || "")
-        @pull.consume_line
+        ext_parser.cell.try { |cell| builder.cell cell }
+
         n_atoms.times do
-          @pull.consume_token
-          ele = PeriodicTable[@pull.int? || @pull.str]? || @pull.error("Unknown element")
-          vec = Spatial::Vec3.new @pull.next_f, @pull.next_f, @pull.next_f
-          builder.atom ele, vec
+          constraint = Constraint::None
+          chain = resname = resid = name = serial = typename = mass = vdw_radius = nil
+          ele = PeriodicTable::C
+          pos = Spatial::Vec3.zero
+          formal_charge = 0
+          partial_charge = temperature_factor = 0.0
+          metadata = Metadata.new
+          occupancy = 1.0
+
           @pull.consume_line
+          ext_parser.fields.each do |field|
+            case field.name
+            when "species"
+              @pull.consume_token
+              ele = PeriodicTable[@pull.int? || @pull.str]? ||
+                    @pull.error("Unknown element")
+            when "pos"
+              pos = Spatial::Vec3.new @pull.next_f, @pull.next_f, @pull.next_f
+            when "chain"
+              chain = @pull.consume_token.expect(/^[a-zA-Z0-9]$/).char
+            when "constraint"
+              case {@pull.next_bool, @pull.next_bool, @pull.next_bool}
+              when {true, true, true}    then constraint = Constraint::XYZ
+              when {false, true, true}   then constraint = Constraint::YZ
+              when {true, false, true}   then constraint = Constraint::XZ
+              when {true, true, false}   then constraint = Constraint::XY
+              when {false, false, true}  then constraint = Constraint::Z
+              when {false, true, false}  then constraint = Constraint::Y
+              when {true, false, false}  then constraint = Constraint::X
+              when {false, false, false} then constraint = Constraint::None
+              end
+            when "charge"
+              if field.type == Int32
+                formal_charge = @pull.next_i
+              else
+                partial_charge = @pull.next_f
+              end
+            when "formal_charge"
+              formal_charge = @pull.next_i
+            when "mass"
+              mass = @pull.next_f
+            when "name"
+              name = @pull.next_s
+            when "occupancy"
+              occupancy = @pull.next_f
+            when "partial_charge"
+              partial_charge = @pull.next_f
+            when "resid"
+              resid = @pull.next_i
+            when "resname"
+              resname = @pull.next_s
+            when "serial"
+              serial = @pull.next_i
+            when "temperature_factor", "bfactor"
+              temperature_factor = @pull.next_f
+            when "typename"
+              typename = @pull.next_s
+            when "vdw_radius"
+              vdw_radius = @pull.next_f
+            else
+              value = if field.cols > 1
+                        (0...field.cols).map { @pull.consume_token.parse(field.type) }
+                      else
+                        @pull.consume_token.parse(field.type)
+                      end
+              metadata[field.name] = value
+            end
+          end
+
+          chain.try { |ch| builder.chain ch }
+          if i = resid
+            builder.residue resname || "UNK", i
+          elsif resname && builder.current_residue.try(&.name) != resname
+            builder.residue resname
+          end
+
+          atom = builder.atom ele, pos
+          {% for name in %w(constraint formal_charge mass name occupancy
+                           partial_charge serial temperature_factor typename
+                           vdw_radius) %}
+            atom.{{name.id}} = {{name.id}} if {{name.id}}
+          {% end %}
+          atom.metadata.merge! metadata
         end
+        @pull.consume_line
       end
+
+      ext_parser.title.try { |title| structure.title = title }
+      structure.metadata.merge! ext_parser.metadata
+
+      structure
     end
 
     def skip_entry : Nil
@@ -43,20 +131,179 @@ module Chem::XYZ
     end
   end
 
-  private class ExtendedParser
-    private record Column,
+  class Writer
+    include FormatWriter(AtomCollection)
+    include FormatWriter::MultiEntry(AtomCollection)
+
+    private TYPE_CHAR_MAP = {Bool => 'L', Int32 => 'I', Float64 => 'R', String => 'S'}
+
+    def initialize(@io : IO,
+                   @extended : Bool = false,
+                   @fields : Array(String) = [] of String,
+                   @total_entries : Int32? = nil,
+                   @sync_close : Bool = false)
+    end
+
+    protected def encode_entry(obj : AtomCollection) : Nil
+      @io.puts obj.n_atoms
+      if obj.is_a?(Structure)
+        if @extended
+          @fields = obj.atoms.first.metadata.keys if @fields.empty?
+          write_extended_config(obj)
+        else
+          @io.puts obj.title.gsub(/ *\n */, ' ')
+        end
+      else
+        @io.puts
+      end
+
+      obj.each_atom do |atom|
+        @io.printf "%-2s %8.3f %8.3f %8.3f", atom.element.symbol, atom.x, atom.y, atom.z
+        @fields.each do |name|
+          @io << ' '
+          case name
+          when "chain"
+            @io << atom.chain.id
+          when "constraint"
+            {:x, :y, :z}.each_with_index do |axis, i|
+              flag = atom.constraint.try(&.includes?(axis)) || false
+              @io << ' ' if i > 0
+              @io << (flag ? 'T' : 'F')
+            end
+          when "formal_charge", "charge"
+            @io.printf "%2d", atom.formal_charge
+          when "mass"
+            @io.printf "%8.4f", atom.mass
+          when "name"
+            @io.printf "%-4s", atom.name
+          when "occupancy"
+            @io.print "%6.2f", atom.occupancy
+          when "partial_charge"
+            @io.printf "%8.4f", atom.partial_charge
+          when "resid"
+            @io.printf "%4d", atom.residue.number
+          when "resname"
+            @io.printf "%-4s", atom.residue.name
+          when "serial"
+            @io.printf "%5d", atom.serial
+          when "temperature_factor", "bfactor"
+            @io.printf "%6.2f", atom.temperature_factor
+          when "typename"
+            @io.printf "%4s", atom.typename
+          when "vdw_radius"
+            @io.printf "%8.3f", atom.vdw_radius
+          else
+            value = atom.metadata[name]? ||
+                    raise "Property #{name} not found in metadata of #{atom}"
+            (value.as_a? || {value}).each do |ele|
+              case type = ele.raw.class
+              when Bool.class    then @io << (ele.raw ? 'T' : 'F')
+              when Int32.class   then @io.printf "%5d", ele.raw
+              when Float64.class then @io.printf "%8g", ele.raw
+              when String.class
+                if ele.as_s.strip.count(&.whitespace?) > 0
+                  raise "Cannot write string with whitespace in extended XYZ"
+                end
+                @io << ele.as_s.strip
+              else
+                raise "Cannot write #{type} in extended XYZ"
+              end
+            end
+          end
+        end
+        @io.puts
+      end
+    end
+
+    private def write_atom_properties(metadata : Metadata) : Nil
+      @io << "species:S:1" << ':' << "pos:R:3"
+      @fields.each do |name|
+        @io << ':' << name << ':'
+        case name
+        when "chain"                         then @io << "S:1"
+        when "constraint"                    then @io << "L:3"
+        when "formal_charge", "charge"       then @io << "I:1"
+        when "mass"                          then @io << "F:1"
+        when "name"                          then @io << "S:1"
+        when "occupancy"                     then @io << "F:1"
+        when "partial_charge"                then @io << "F:1"
+        when "resid"                         then @io << "I:1"
+        when "resname"                       then @io << "S:1"
+        when "serial"                        then @io << "I:1"
+        when "temperature_factor", "bfactor" then @io << "F:1"
+        when "typename"                      then @io << "S:1"
+        when "vdw_radius"                    then @io << "F:1"
+        else
+          if value = metadata[name]?
+            if arr = value.as_a?
+              raise ArgumentError.new("Nested arrays are not supported") if arr[0].as_a?
+              type = arr[0].raw.class
+              cols = arr.size
+            else
+              type = value.raw.class
+              cols = 1
+            end
+
+            type = TYPE_CHAR_MAP[type]? || raise "Cannot write #{type} in extended XYZ"
+            @io << type << ':' << cols
+          else
+            raise ArgumentError.new("Property #{name} not found in atom metadata")
+          end
+        end
+      end
+    end
+
+    private def write_extended_config(structure : Structure) : Nil
+      @io << "Title="
+      structure.title.gsub(/ *\n */, ' ').inspect @io
+      @io << ' ' << "Properties="
+      write_atom_properties structure.atoms.first.metadata
+      if cell = structure.cell?
+        @io << ' ' << "Lattice=" << '['
+        cell.basisvec.each_with_index do |(x, y, z), i|
+          @io << ", " if i > 0
+          @io << '['
+          {x, y, z}.join(@io, ", ") { |i, io| io << i }
+          @io << ']'
+        end
+        @io << ']'
+      end
+      structure.metadata.each do |key, value|
+        @io << ' ' << key.camelcase << '='
+        value.raw.inspect @io
+      end
+      @io.puts
+    end
+  end
+
+  private class ConfigurationParser
+    record Field,
       name : String,
       type : Int32.class | Float64.class | String.class | Bool.class,
-      width : Int32
+      cols : Int32 = 1
+
+    TYPE_CHARS  = {'S' => String, 'I' => Int32, 'R' => Float64, 'L' => Bool}
+    FIELD_SPECS = {
+      "chain"      => "chain:S:1",
+      "charge"     => %w(charge:I:1 charge:F:1),
+      "constraint" => "constraint:L:3",
+      "pos"        => "pos:R:3",
+      "resid"      => "resid:I:1",
+      "resname"    => "resname:S:1",
+      "species"    => "species:S:1",
+    }
+
+    getter cell : Spatial::Parallelepiped?
+    getter fields = [] of Field
+    getter metadata = Metadata.new
+    getter title = ""
 
     def initialize(@pull : PullParser)
     end
 
-    def find_consume_token(delim : String = " \t=") : Bool
+    def consume_token(delim : String = " \t=") : Nil
       @pull.skip_whitespace
-      return false unless quote = @pull.peek
-
-      if quote && quote.in?('"', "'")
+      if (quote = @pull.peek) && quote.in?('"', "'")
         quotes = 0
         @pull.consume do |char|
           if quotes < 2
@@ -71,47 +318,101 @@ module Chem::XYZ
       else
         @pull.consume &.in?(delim).!
       end
-      true
     end
 
-    def parse : {Metadata?, Array(Column)}
-      cols = [] of Column
-      metadata = Metadata.new
-
-      return {metadata, cols} unless @pull.line.includes?("Properties=")
-
-      while find_consume_token
-        name = @pull.str
-        if @pull.skip_whitespace.peek == '='
-          @pull.consume(1).expect('=').skip_whitespace
-          if @pull.peek.in?('{', '[')
-            value = parse_array
-          else
-            find_consume_token
-            value = parse_value
-          end
-        else # properties by themselves are implicitly set to true
-          value = true
+    def make_cell(value : Metadata::Any) : Spatial::Parallelepiped
+      if vecs = value.as_2a?(Float64)
+        if vecs.size == 3 && vecs.all?(&.size.==(3))
+          i, j, k = vecs
+          i = Spatial::Vec3[i[0], i[1], i[2]]
+          j = Spatial::Vec3[j[0], j[1], j[2]]
+          k = Spatial::Vec3[k[0], k[1], k[2]]
+          Spatial::Parallelepiped.new i, j, k
+        else
+          @pull.error "Invalid cell basis vectors"
         end
-        puts "#{name} => #{value.inspect}"
+      elsif params = value.as_a?(Float64)
+        case params.size
+        when 6
+          size = {params[0], params[1], params[2]}
+          angles = {params[3], params[4], params[5]}
+          Spatial::Parallelepiped.new size, angles
+        when 9
+          i = Spatial::Vec3[params[0], params[1], params[2]]
+          j = Spatial::Vec3[params[3], params[4], params[5]]
+          k = Spatial::Vec3[params[6], params[7], params[8]]
+          Spatial::Parallelepiped.new i, j, k
+        else
+          @pull.error "Invalid cell parameters"
+        end
+      elsif str = value.as_s?
+        params = str.split.map do |x|
+          x.to_f? || @pull.error "Invalid cell value #{x.inspect}"
+        end
+        make_cell Metadata::Any.new(params)
+      else
+        @pull.error "Invalid cell parameters"
       end
-      {metadata, cols}
     end
 
-    def parse_array
+    def parse : Nil
+      if @pull.line!.includes?("Properties=")
+        until @pull.skip_whitespace.eol?
+          consume_token
+          name = @pull.str.underscore
+          if @pull.skip_whitespace.peek == '='
+            @pull.consume(1).expect('=').skip_whitespace
+            if name == "properties"
+              parse_atom_field_spec
+              next
+            elsif @pull.peek.in?('{', '[')
+              value = parse_array
+            else
+              consume_token
+              value = parse_value
+            end
+          else # properties by themselves are implicitly set to true
+            value = true
+          end
+
+          case name
+          when "lattice"
+            @cell = make_cell Metadata::Any.new(value)
+          when "title"
+            @title = value.as(String)
+          else
+            @metadata[name.underscore] = value
+          end
+        end
+      else
+        @fields << Field.new("species", String) << Field.new("pos", Float64, 3)
+        @title = @pull.line!.strip
+      end
+    end
+
+    def parse_array : Array(Metadata::ValueType) | Array(Array(Metadata::ValueType))
       open_array_delim = @pull.consume(1).char
       new_style = open_array_delim == '['
       close_array_delim = new_style ? ']' : '}'
       value_delim = " \t#{close_array_delim}#{',' if new_style}"
 
-      if !new_style && @pull.skip_whitespace.peek.in?('{', '[')
+      case @pull.skip_whitespace.peek
+      when '['
+        if new_style
+          arrays = parse_arrays
+          @pull.consume(1).expect ']', "Unterminated array"
+          return arrays
+        else
+          @pull.error "Invalid nested array (use [[...]] syntax)"
+        end
+      when '{'
         @pull.error "Invalid nested array (use [[...]] syntax)"
       end
 
-      Array(Metadata::ValueType).new.tap do |array|
+      Array(Metadata::ValueType).new.tap do |arr|
         loop do
-          find_consume_token value_delim
-          array << parse_value
+          consume_token value_delim
+          arr << parse_value
           break if @pull.skip_whitespace.peek.in?(nil, close_array_delim)
           @pull.consume(1).expect(',') if new_style
         end
@@ -119,31 +420,72 @@ module Chem::XYZ
       end
     end
 
-    def parse_value : Metadata::ValueType
-      @pull.int?.try { |int| return int }
-      @pull.float?.try { |float| return float }
-
-      case (str = @pull.str).downcase
-      when "t", "true"
-        true
-      when "f", "false"
-        false
-      else
-        str.strip("\"'")
+    def parse_arrays : Array(Array(Metadata::ValueType))
+      Array(Array(Metadata::ValueType)).new.tap do |arr|
+        loop do
+          case @pull.skip_whitespace.peek
+          when ']'
+            break
+          when '['
+            arr << parse_array.as(Array(Metadata::ValueType))
+          when ','
+            @pull.consume(1)
+          else
+            @pull.error "Unexpected token #{@pull.str?.inspect}"
+          end
+        end
       end
     end
-  end
 
-  class Writer
-    include FormatWriter(AtomCollection)
-    include FormatWriter::MultiEntry(AtomCollection)
+    def parse_atom_field_spec : Nil
+      @pull.consume(1) if @pull.peek == '"'
+      while (char = @pull.peek) && !char.in?(' ', '\t', '"')
+        consume_token(delim: ":\"")
+        name = @pull.str("Expected column name")
 
-    protected def encode_entry(obj : AtomCollection) : Nil
-      @io.puts obj.n_atoms
-      @io.puts obj.is_a?(Structure) ? obj.title.gsub(/ *\n */, ' ') : ""
-      obj.each_atom do |atom|
-        @io.printf "%-3s%15.5f%15.5f%15.5f\n", atom.element.symbol, atom.x, atom.y, atom.z
+        @pull.consume(1).expect(':')
+
+        @pull.consume(1).expect(TYPE_CHARS.keys)
+        type_char = @pull.char
+        type = TYPE_CHARS[type_char]
+
+        @pull.consume(1).expect(':')
+
+        consume_token(delim: ":\" ")
+        cols = @pull.int("Invalid number of columns")
+
+        # check field spec if predefined
+        FIELD_SPECS[name]?.try do |expected|
+          actual = "#{name}:#{type_char}:#{cols}"
+          case expected
+          in Array
+            unless actual.in?(expected)
+              expected = expected.sentence(
+                pair_separator: " or ",
+                tail_separator: ", or ")
+              @pull.error("Expected #{expected}, got #{actual}")
+            end
+          in String
+            @pull.error("Expected #{expected}, got #{actual}") unless actual == expected
+          end
+        end
+
+        @fields << Field.new(name, type, cols)
+        @pull.consume(1) if @pull.peek == ':'
       end
+      @pull.consume(1) if @pull.peek == '"'
+
+      field_names = @fields.map(&.name)
+      unless "species".in?(field_names) && "pos".in?(field_names)
+        @pull.error "Properties do not contain 'species:S:1' nor 'pos:R:3'"
+      end
+    end
+
+    def parse_value : Int32 | Float64 | String | Bool
+      @pull.int?.try { |int| return int }
+      @pull.float?.try { |float| return float }
+      @pull.bool?.try { |bool| return bool }
+      @pull.str.strip("\"'")
     end
   end
 end
