26ce80bda76500d1b674587d3790d919ae1120ee
Mauricio Bedoya <62667433+maurobedoya@users.noreply.github.com>
2023-10-07 20:18:26 -0300

    Refactor spec/spatial/coordinates_proxy_spec.cr
    Co-authored-by: Francisco Adasme <franciscoadasme@users.noreply.github.com>

--- spec/spatial/coordinates_proxy_spec.cr
+++ spec/spatial/coordinates_proxy_spec.cr
@@ -195,7 +195,7 @@ describe Chem::Spatial::CoordinatesProxy do
   describe ".rdgyr" do 
     it "computes the rdgyr in-place" do 
       s = Chem::Structure.read spec_file("FAD_can_prep.pdb")
-      s.coords.rdgyr().should be_close 6.339020, 1e-6
+      s.coords.rdgyr.should be_close 6.339020, 1e-6
     end
   end
 
