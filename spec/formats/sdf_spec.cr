e777be28b2128f7084654019fcf3a0a0a9bf8535
Francisco Adasme <francisco.adasme@gmail.com>
2023-10-18 14:32:33 -0300

    fix(io.sdf): Read data items as metadata

--- spec/formats/sdf_spec.cr
+++ spec/formats/sdf_spec.cr
@@ -11,6 +11,16 @@ describe Chem::SDF::Reader do
       structure.atoms.size.should eq 47
       structure.bonds.size.should eq 50
       # SDF uses Mol behind the scenes so no need to do further checks
+
+      structure.metadata.keys.should eq %w(cid energy)
+      structure.metadata["cid"].raw.should be_a Int32
+      structure.metadata["energy"].raw.should be_a Float64
     end
+
+    structures[0].metadata["cid"].should eq 42
+    structures[0].metadata["energy"].should eq -69.2498879662534
+
+    structures[86].metadata["cid"].should eq 95
+    structures[86].metadata["energy"].should eq -66.80468478035203
   end
 end
--- src/chem/formats/sdf.cr
+++ src/chem/formats/sdf.cr
@@ -10,8 +10,26 @@ module Chem::SDF
 
     private def decode_entry : Structure
       structure = Mol::Reader.new(@pull).read_entry
-      skip_after_delimiter
+      structure.metadata.merge! parse_metadata
+      @pull.expect("$$$$").consume_line
       structure
+    end
+
+    private def parse_metadata : Metadata
+      Chem::Metadata.new.tap do |metadata|
+        while @pull.consume_line.next_s? == ">"
+          key = @pull.expect_next(/<\w+>/).str.lchop('<').rchop('>').underscore
+          value = @pull.consume_line.line.presence || @pull.error "Expected a data value for field #{key}"
+          if value.size == 200 # may be split into multiple lines
+            while @pull.consume_line.line.presence
+              value += @pull.line || ""
+            end
+          elsif !@pull.consume_line.line.try(&.blank?)
+            @pull.error "Expected blank line after field #{key}"
+          end
+          metadata[key] = value.to_i? || value.to_f? || value
+        end
+      end
     end
 
     def skip_entry : Nil
