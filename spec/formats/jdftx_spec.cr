4cc8fb79c5d9487718fdd19be07cbc341d0b845d
Francisco Adasme <francisco.adasme@gmail.com>
2023-12-15 15:19:53 -0300

    feat(io.jdftx): Read/write JDFTx geometry files

---
+++ spec/formats/jdftx_spec.cr
@@ -0,0 +1,66 @@
+require "../spec_helper"
+
+describe Chem::JDFTx::Reader do
+  it "parses a JDFTx file" do
+    path = spec_file("polyala-val.ionpos")
+    structure = Chem::Structure.from_jdftx path, guess_bonds: true, guess_names: true
+
+    structure.residues.size.should eq 6
+    structure.residues.map(&.name).should eq %w(VAL ALA ALA ALA ALA ALA)
+    structure.dig('A', 1, "HG13").coords.should be_close vec3(-3.632, 1.758, 1.855), 1e-3
+
+    structure.cell.basisvec.should be_close [
+      vec3(21.023, 0, 0),
+      vec3(0, 21.056, 0),
+      vec3(0, 0, 21.162),
+    ], 1e-3
+  end
+
+  it "parses a split JDFTx file" do
+    path = spec_file("ff2d.ionpos")
+    structure = Chem::Structure.from_jdftx path
+    structure.atoms[5].coords.should be_close vec3(-9.997, -3.330, 3.142), 1e-3
+
+    structure.topology.guess_bonds
+    structure.topology.guess_formal_charges
+    structure.topology.guess_names
+    structure.residues.size.should eq 36
+    structure.residues.map(&.name).uniq.should eq %w(PHE)
+
+    structure.cell.basisvec.should be_close [
+      vec3(23.84396309, 0.00112639, 0.00055391),
+      vec3(-11.92104721, 20.67853790, -0.00008092),
+      vec3(-0.00082321, -0.00033590, 35.43662857),
+    ], 1e-8
+  end
+end
+
+describe Chem::JDFTx::Writer do
+  it "writes a JDFTx file" do
+    reduced_cell_prec = ->(str : String) {
+      str.gsub(/\.(\d{3})\d{12}/, ".\\1000000000000")
+    }
+    path = spec_file("polyala-val.ionpos")
+    expected = reduced_cell_prec.call File.read(path)
+    actual = reduced_cell_prec.call Chem::Structure.read(path).to_jdftx
+    actual.should eq expected
+  end
+
+  it "writes a split JDFTx file" do
+    reduced_cell_prec = ->(str : String) {
+      str.gsub(/\.(\d{3})\d{12}/, ".\\1000000000000")
+    }
+
+    path = Path[spec_file("ff2d.ionpos")]
+    tmp = File.tempfile(".ionpos") do |io|
+      Chem::Structure.read(path).to_jdftx io, single_file: false
+    end
+
+    File.read(tmp.path).should eq File.read(path)
+    expected = reduced_cell_prec.call File.read(path.with_ext(".lattice"))
+    actual = reduced_cell_prec.call File.read(Path[tmp.path].with_ext(".lattice"))
+    actual.should eq expected
+  ensure
+    tmp.try &.delete
+  end
+end
---
+++ src/chem/formats/jdftx.cr
@@ -0,0 +1,143 @@
+@[Chem::RegisterFormat(ext: %w(.ionpos .lattice))]
+module Chem::JDFTx
+  class Reader
+    include FormatReader(Structure)
+
+    def initialize(
+      @io : IO,
+      @guess_bonds : Bool = false,
+      @guess_names : Bool = false,
+      @sync_close : Bool = false
+    )
+    end
+
+    protected def decode_entry : Structure
+      fractional = false
+      lattice_scale = {1.0, 1.0, 1.0}
+      cell = nil
+      structure = Structure.build(
+        guess_bonds: @guess_bonds,
+        guess_names: @guess_names,
+        source_file: (file = @io).is_a?(File) ? file.path : nil,
+        use_templates: false,
+      ) do |builder|
+        @io.gets_to_end.gsub(/\s*\\\s*\n\s*/, ' ').each_line do |line|
+          tokens = line.split
+          case tokens.first?
+          when "ion"
+            element = PeriodicTable[tokens[1]]
+            pos = Spatial::Vec3[*tokens[{2, 3, 4}].map(&.to_f)]
+            pos = pos.map(&.bohrs) unless fractional
+            atom = builder.atom element, pos
+            tokens.delete_at 5..8 if tokens[5] == "v" # ignore velocity if present
+            atom.constraint = :xyz if tokens[5].to_f == 0
+          when "coords-type"
+            fractional = tokens[1].downcase == "lattice"
+          when "latt-scale"
+            lattice_scale = tokens[{1, 2, 3}].map &.to_f
+          when "lattice"
+            cell = parse_cell(line)
+          end
+        end
+
+        if file = @io.as?(File)
+          path = Path[file.path]
+          path = path.sibling "#{path.stem}.lattice"
+          if File.exists?(path)
+            cell = parse_cell File.read(path).gsub(/\s*\\\s*\n\s*/, ' ')
+          end
+        end
+
+        builder.cell cell if cell
+      end
+
+      cell = cell.try &.scale(*lattice_scale)
+      if fractional
+        raise "Missing command lattice" unless cell
+        structure.coords.to_cart!
+      end
+
+      structure
+    end
+
+    private def parse_cell(line : String) : Spatial::Parallelepiped
+      tokens = line.split
+      case tokens[1].downcase
+      when "monoclinic"
+        a, _, c = tokens[1..3].map(&.to_f.bohrs)
+        beta = tokens[4].to_f
+        Spatial::Parallelepiped.monoclinic(a, c, beta)
+      when "triclinic"
+        lengths = {1, 2, 3}.map { |i| tokens[i].to_f.bohrs }
+        angles = {4, 5, 6}.map { |i| tokens[i].to_f }
+        Spatial::Parallelepiped.new(lengths, angles)
+      when "orthorhombic"
+        a, b, c = tokens[1..3].map(&.to_f.bohrs)
+        Spatial::Parallelepiped.orthorhombic(a, b, c)
+      when "tetragonal"
+        a, c = tokens[1..2].map(&.to_f.bohrs)
+        Spatial::Parallelepiped.tetragonal(a, c)
+      when "hexagonal"
+        a, c = tokens[1..2].map(&.to_f.bohrs)
+        Spatial::Parallelepiped.hexagonal(a, c)
+      when "rhombohedral"
+        a = tokens[1].to_f.bohrs
+        alpha = tokens[2].to_f
+        Spatial::Parallelepiped.rhombohedral(a, alpha)
+      when "cubic"
+        size = tokens[1].to_f.bohrs
+        Spatial::Parallelepiped.cubic(size)
+      else
+        values = tokens[1..9].map(&.to_f.bohrs)
+        raise "Invalid command lattice" unless values.size == 9
+        bi, bj, bk = { {0, 1, 2}, {3, 4, 5}, {6, 7, 8} }.map do |idxs|
+          Spatial::Vec3[*values[idxs]]
+        end
+        basis = Spatial::Mat3.basis bi, bj, bk
+        Spatial::Parallelepiped.new(basis)
+      end
+    end
+  end
+
+  class Writer
+    include FormatWriter(Structure)
+
+    def initialize(
+      @io : IO,
+      @single_file : Bool = true,
+      @sync_close : Bool = false
+    )
+    end
+
+    def encode_entry(obj : Chem::Structure) : Nil
+      io = @io
+      close_file = false
+      if io.is_a?(File) && !@single_file
+        io = File.open Path[io.path].with_ext(".lattice"), "w"
+        close_file = true
+      end
+
+      io.puts "lattice \\"
+      obj.cell.basisvec.each_with_index do |vec, i|
+        io.printf "    %21.15f%21.15f%21.15f",
+          vec.x.to_bohrs,
+          vec.y.to_bohrs,
+          vec.z.to_bohrs
+        io.puts " \\" if i < 2
+      end
+      io.puts
+
+      io.close if close_file
+
+      @io.puts "coords-type Cartesian"
+      obj.atoms.each do |atom|
+        @io.printf "ion%4s%8.3f%8.3f%8.3f%2d\n",
+          atom.element.symbol,
+          atom.x.to_bohrs,
+          atom.y.to_bohrs,
+          atom.z.to_bohrs,
+          atom.constraint.try(&.xyz?) ? 0 : 1
+      end
+    end
+  end
+end
