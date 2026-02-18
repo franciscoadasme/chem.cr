23b20e2c537d8a9f7cd76a5203511649f1983fc6
Francisco Adasme <francisco.adasme@gmail.com>
2026-02-18 15:59:52 -0300

    refactor(io): Move Reader/Writer logic into read/write methods in Mol2

--- src/chem/formats/mol2.cr
+++ src/chem/formats/mol2.cr
@@ -1,9 +1,20 @@
 @[Chem::RegisterFormat(ext: %w(.mol2), module_api: true)]
 module Chem::Mol2
   # Yields each structure in *io*.
-  def self.each(io : IO | Path | String, & : Structure ->) : Nil
-    Reader.open(io) do |reader|
-      reader.each do |struc|
+  def self.each(io : IO, & : Structure ->) : Nil
+    loop do
+      begin
+        yield read(io)
+      rescue IO::EOFError
+        break
+      end
+    end
+  end
+
+  # :ditto:
+  def self.each(path : Path | String, & : Structure ->) : Nil
+    File.open(path) do |file|
+      each(file) do |struc|
         yield struc
       end
     end
@@ -11,222 +22,200 @@ module Chem::Mol2
 
   # Returns the first structure from *io*.
   # Use `read_all` or `each` for multiple.
-  def self.read(io : IO | Path | String) : Structure
-    Reader.open(io) do |reader|
-      reader.read_entry
+  def self.read(io : IO) : Structure
+    pull = PullParser.new(io)
+
+    pull.each_line do
+      break if (pull.str? || pull.next_s?) == "@<TRIPOS>MOLECULE"
+    end
+    pull.consume_line
+    raise IO::EOFError.new if pull.eof?
+
+    title = pull.line!.strip
+    pull.consume_line
+    n_atoms = pull.next_i
+    n_bonds = pull.next_i
+    pull.consume_line
+    pull.consume_line
+    include_charges = pull.next_s != "NO_CHARGES"
+    pull.consume_line
+
+    struc = Structure.build(
+      guess_bonds: false,
+      guess_names: false,
+      source_file: (file = pull.io).is_a?(File) ? file.path : nil,
+      use_templates: false,
+    ) do |builder|
+      builder.title title
+      pull.each_line do
+        case pull.str? || pull.next_s?
+        when "@<TRIPOS>ATOM"
+          n_atoms.times do
+            pull.consume_line
+            number = pull.next_i
+            name = pull.next_s
+            pos = Spatial::Vec3[pull.next_f, pull.next_f, pull.next_f]
+            atom_t = pull.next_s
+            symbol = atom_t[...atom_t.index('.')] # ignore sybyl type
+            element = PeriodicTable[symbol]? || pull.error("Unknown element")
+            unless pull.consume_token.eol?
+              resid = pull.int
+              resname = pull.next_s
+              # TODO: respect name or truncate at 4 characters?
+              builder.residue resname[..2], resid
+              chg = pull.next_f if include_charges
+            end
+            builder.atom name, pos, element: element, partial_charge: (chg || 0.0)
+          end
+        when "@<TRIPOS>BOND"
+          n_bonds.times do
+            pull.consume_line
+            pull.consume_token # skip bond index
+            i = pull.next_i
+            j = pull.next_i
+            case bond_t = pull.next_s
+            when "1", "2", "3"
+              builder.bond i, j, BondOrder.from_value(bond_t.to_i)
+            when "ar"
+              builder.bond i, j, aromatic: true
+            when "am", "du"
+              builder.bond i, j
+            end
+          end
+        when "@<TRIPOS>CRYSIN"
+          pull.consume_line
+          x = pull.next_f
+          pull.error "Invalid size" unless x > 0
+          y = pull.next_f
+          pull.error "Invalid size" unless y > 0
+          z = pull.next_f
+          pull.error "Invalid size" unless z > 0
+          alpha = pull.next_f
+          pull.error "Invalid angle" unless 0 < alpha <= 180
+          beta = pull.next_f
+          pull.error "Invalid angle" unless 0 < beta <= 180
+          gamma = pull.next_f
+          pull.error "Invalid angle" unless 0 < gamma <= 180
+          builder.cell Spatial::Parallelepiped.new({x, y, z}, {alpha, beta, gamma})
+        when "@<TRIPOS>MOLECULE"
+          # FIXME: hack such that the next call to read can start at this line
+          if line = pull.line
+            io.pos -= line.bytesize + 1
+          end
+          break
+        end
+      end
+    end
+    struc.guess_formal_charges
+    struc
+  end
+
+  # :ditto:
+  def self.read(path : Path | String) : Structure
+    File.open(path) do |file|
+      read(file)
     end
   end
 
   # Returns all structures in *io*.
-  def self.read_all(io : IO | Path | String) : Array(Structure)
-    Reader.open(io) do |reader|
-      ary = [] of Structure
-      reader.each { |struc| ary << struc }
-      ary
+  def self.read_all(io : IO) : Array(Structure)
+    ary = [] of Structure
+    each(io) do |struc|
+      ary << struc
+    end
+    ary
+  end
+
+  # :ditto:
+  def self.read_all(path : Path | String) : Array(Structure)
+    File.open(path) do |file|
+      read_all(file)
     end
   end
 
   # Writes one or more structures or groups of atoms to *io*.
-  def self.write(io : IO | Path | String, obj : Structure | AtomView) : Nil
-    Writer.open(io) do |writer|
-      writer << obj
+  def self.write(io : IO, obj : AtomView | Structure) : Nil
+    raise Error.new("Structure has no bonds") if obj.bonds.empty?
+
+    atoms = obj.is_a?(AtomView) ? obj : obj.atoms
+    atom_table = atoms.each.with_index(offset: 1).to_h
+    res_table = obj.residues.each.with_index(offset: 1).to_h
+
+    write_section(io, "molecule") do
+      io.puts obj.is_a?(Structure) ? obj.title.gsub(/ *\n */, ' ') : ""
+      io.printf "%5d%5d%4d\n", atoms.size, obj.bonds.size, obj.residues.size
+      io.puts "UNKNOWN"
+      io.puts "USER_CHARGES"
+    end
+    write_section(io, "atom") do
+      atoms.each do |atom|
+        io.printf "%5d %-4s%10.4f%10.4f%10.4f %-4s%4d %3s%-4d%8.4f\n",
+          atom_table[atom],
+          atom.name,
+          atom.x, atom.y, atom.z,
+          atom.element.symbol,
+          res_table[atom.residue],
+          atom.residue.name,
+          atom.residue.number,
+          atom.partial_charge
+      end
+    end
+    write_section(io, "bond") do
+      obj.bonds.each_with_index do |bond, i|
+        io.printf "%5d%5d%5d%2d\n",
+          i + 1,
+          atom_table[bond.atoms[0]],
+          atom_table[bond.atoms[1]],
+          bond.order
+      end
+    end
+    write_section(io, "substructure") do
+      obj.residues.each do |residue|
+        root_atom = residue.protein? ? residue.dig("CA") : residue.atoms[0]
+        io.printf "%4d %-8s %5d %-8s %1s %1s %3s\n",
+          res_table[residue],
+          "#{residue.name[..2]}#{residue.number}",
+          atom_table[root_atom],
+          "RESIDUE",
+          residue.protein? ? 1 : '*',
+          residue.chain.id,
+          residue.name[..2]
+      end
+    end
+
+    if (struc = obj.as?(Structure)) && (cell = struc.cell?)
+      write_section(io, "crysin") do
+        a, b, c = cell.size
+        alpha, beta, gamma = cell.angles
+        io.printf "%.3f %.3f %.3f %.2f %.2f %.2f 1 1\n", a, b, c, alpha, beta, gamma
+      end
     end
   end
 
   # :ditto:
-  def self.write(io : IO | Path | String, objs : Enumerable(Structure)) : Nil
-    Writer.open(io) do |writer|
-      objs.each { |struc| writer << struc }
+  def self.write(path : Path | String, obj : Structure | AtomView) : Nil
+    File.open(path, "w") do |file|
+      write(file, obj)
     end
   end
 
-  class Reader
-    include FormatReader(Structure)
-    include FormatReader::MultiEntry(Structure)
-
-    def initialize(@io : IO, @sync_close : Bool = false)
-      @pull = PullParser.new @io
-    end
-
-    def skip_entry : Nil
-      @pull.consume_line if @pull.next_s == "@<TRIPOS>MOLECULE"
-      skip_to_tag "@<TRIPOS>MOLECULE"
-    end
-
-    private def decode_entry : Structure
-      skip_to_tag "@<TRIPOS>MOLECULE"
-      @pull.consume_line
-      raise IO::EOFError.new if @pull.eof?
-
-      title = @pull.line!.strip
-      @pull.consume_line
-      n_atoms = @pull.next_i
-      n_bonds = @pull.next_i
-      @pull.consume_line
-      @pull.consume_line
-      include_charges = @pull.next_s != "NO_CHARGES"
-      @pull.consume_line
-
-      Structure.build(
-        guess_bonds: false,
-        guess_names: false,
-        source_file: (file = @io).is_a?(File) ? file.path : nil,
-        use_templates: false,
-      ) do |builder|
-        builder.title title
-        @pull.each_line do
-          case @pull.str? || @pull.next_s?
-          when "@<TRIPOS>ATOM"
-            n_atoms.times do
-              @pull.consume_line
-              number = @pull.next_i
-              name = @pull.next_s
-              pos = Spatial::Vec3[@pull.next_f, @pull.next_f, @pull.next_f]
-              atom_t = @pull.next_s
-              symbol = atom_t[...atom_t.index('.')] # ignore sybyl type
-              element = PeriodicTable[symbol]? || @pull.error("Unknown element")
-              unless @pull.consume_token.eol?
-                resid = @pull.int
-                resname = @pull.next_s
-                # TODO: respect name or truncate at 4 characters?
-                builder.residue resname[..2], resid
-                chg = @pull.next_f if include_charges
-              end
-              builder.atom name, pos, element: element, partial_charge: (chg || 0.0)
-            end
-          when "@<TRIPOS>BOND"
-            n_bonds.times do
-              @pull.consume_line
-              @pull.consume_token # skip bond index
-              i = @pull.next_i
-              j = @pull.next_i
-              case bond_t = @pull.next_s
-              when "1", "2", "3"
-                builder.bond i, j, BondOrder.from_value(bond_t.to_i)
-              when "ar"
-                builder.bond i, j, aromatic: true
-              when "am", "du"
-                builder.bond i, j
-              end
-            end
-          when "@<TRIPOS>CRYSIN"
-            @pull.consume_line
-            x = @pull.next_f
-            @pull.error "Invalid size" unless x > 0
-            y = @pull.next_f
-            @pull.error "Invalid size" unless y > 0
-            z = @pull.next_f
-            @pull.error "Invalid size" unless z > 0
-            alpha = @pull.next_f
-            @pull.error "Invalid angle" unless 0 < alpha <= 180
-            beta = @pull.next_f
-            @pull.error "Invalid angle" unless 0 < beta <= 180
-            gamma = @pull.next_f
-            @pull.error "Invalid angle" unless 0 < gamma <= 180
-            builder.cell Spatial::Parallelepiped.new({x, y, z}, {alpha, beta, gamma})
-          when "@<TRIPOS>MOLECULE"
-            break
-          end
-        end
-      end
-        .tap &.guess_formal_charges
-    end
-
-    private def skip_to_tag(tag : String) : Nil
-      @pull.each_line do
-        break if (@pull.str? || @pull.next_s?) == tag
-      end
+  # :ditto:
+  def self.write(io : IO, objs : Enumerable(Structure)) : Nil
+    objs.each do |struc|
+      write(io, struc)
     end
   end
 
-  class Writer
-    include FormatWriter(AtomContainer)
-    include FormatWriter::MultiEntry(AtomContainer)
-
-    @atom_table = {} of Atom => Int32
-    @res_table = {} of Residue => Int32
-
-    protected def encode_entry(obj : AtomContainer) : Nil
-      atoms = obj.is_a?(AtomView) ? obj : obj.atoms
-
-      reset_index
-      raise Error.new("Structure has no bonds") if obj.bonds.empty?
-      write_header obj.is_a?(Structure) ? obj.title : "",
-        atoms.size,
-        obj.bonds.size,
-        obj.is_a?(Structure) ? obj.residues.size : atoms.map(&.residue).uniq.size
-      section "atom" { atoms.each { |atom| write atom } }
-      section "bond" { obj.bonds.each_with_index { |bond, i| write bond, i + 1 } }
-      section "substructure" do
-        obj.residues.each do |residue|
-          root_atom = residue.protein? ? residue.dig("CA") : residue.atoms[0]
-          @io.printf "%4d %-8s %5d %-8s %1s %1s %3s\n",
-            residue_index(residue),
-            "#{residue.name[..2]}#{residue.number}", # subst_name
-            atom_index(root_atom),                   # root atom
-            "RESIDUE",                               # subst_type
-            residue.protein? ? 1 : '*',              # dict_type
-            residue.chain.id,                        # chain
-            residue.name[..2]                        # sub_type
-        end
-      end
-      if (structure = obj.as?(Structure)) && (cell = structure.cell?)
-        section "crysin" do
-          a, b, c = cell.size
-          alpha, beta, gamma = cell.angles
-          formatl "%.3f %.3f %.3f %.2f %.2f %.2f 1 1", a, b, c, alpha, beta, gamma
-        end
-      end
+  # :ditto:
+  def self.write(path : Path | String, objs : Enumerable(Structure)) : Nil
+    File.open(path, "w") do |file|
+      write(file, objs)
     end
+  end
 
-    private def atom_index(atom : Atom) : Int32
-      @atom_table[atom] ||= @atom_table.size + 1
-    end
-
-    private def reset_index
-      @atom_table.clear
-      @res_table.clear
-    end
-
-    private def residue_index(residue : Residue) : Int32
-      @res_table[residue] ||= @res_table.size + 1
-    end
-
-    private def section(name : String, &block : ->)
-      @io << "@<TRIPOS>" << name.upcase << '\n'
-      yield
-      @io.puts
-    end
-
-    private def write(atom : Atom)
-      @io.printf "%5d %-4s%10.4f%10.4f%10.4f %-4s%4d %3s%-4d%8.4f\n",
-        atom_index(atom),
-        atom.name,
-        atom.x,
-        atom.y,
-        atom.z,
-        atom.element.symbol,
-        residue_index(atom.residue),
-        atom.residue.name,
-        atom.residue.number,
-        atom.partial_charge
-    end
-
-    private def write(bond : Bond, i : Int32)
-      @io.printf "%5d%5d%5d%2d\n",
-        i,
-        atom_index(bond.atoms[0]),
-        atom_index(bond.atoms[1]),
-        bond.order
-    end
-
-    private def write_header(title, n_atoms, n_bonds, n_residues)
-      section "molecule" do
-        @io.puts title.gsub(/ *\n */, ' ')
-        @io.printf "%5d%5d%4d\n", n_atoms, n_bonds, n_residues
-        @io.puts "UNKNOWN"
-        @io.puts "USER_CHARGES"
-      end
-    end
+  private def self.write_section(io : IO, name : String, & : ->) : Nil
+    io << "@<TRIPOS>" << name.upcase << '\n'
+    yield
+    io.puts
   end
 end
